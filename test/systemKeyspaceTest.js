c0bd329229f51ff8dd20feddf0f118ad19c38554
jorgebay <jorgebaygondra@gmail.com>
2013-07-03 11:36:17 +0200

    Added unit tests to querying system keyspace

---
+++ test/systemKeyspaceTest.js
@@ -0,0 +1,64 @@
+var Client = require('../index.js').Client;
+var Connection = require('../index.js').Connection;
+var con = new Connection({host:'localhost', port: 9042});;
+//declaration order is execution order in nodeunit
+module.exports = {
+  connect : function (test) {
+    con.on('log', function(type, message) {
+      //console.log(type, message);
+    });
+    con.open(function (err) {
+      test.ifError(err, 'Connection error');
+      test.done();
+    });
+  },
+  selectAllColumns: function (test) {
+    con.execute('select * from system.schema_keyspaces;', [], function(err, result) {
+      test.ok(!err, 'Execute error');
+      if (!err) {
+        test.ok(result.rows.length > 0, 'No keyspaces')
+      }
+      test.done();
+    });
+  },
+  select2Column: function (test) {
+    con.execute('select keyspace_name, strategy_class from system.schema_keyspaces;', [], function(err, result) {
+      test.ok(!err, 'Execute error');
+      if (!err) {
+        test.ok(result.rows.length > 0, 'No keyspaces');
+        test.ok(result.meta.columns.length === 2, 'There must be 2 columns');
+        if (result.rows.length > 0) {
+          test.ok(result.rows[0].get('keyspace_name'), 'Get cell by value failed');
+        }
+      }
+      test.done();
+    });
+  },
+  selectNoMatch: function (test) {
+    con.execute('select * from system.schema_keyspaces where keyspace_name=?;', ['does_not_exist'], function(err, result) {
+      test.ok(!err, 'Execute error');
+      if (!err) {
+        test.ok(result.rows.length === 0, 'It must return an empty result set, with an empty rows array.');
+      }
+      test.done();
+    });
+  },
+  select1Match: function (test) {
+    con.execute('select * from system.schema_keyspaces where keyspace_name=?;', ['system'], function(err, result) {
+      test.ok(!err, 'Execute error');
+      if (!err) {
+        test.ok(result.rows.length === 1, 'It must return an result set with 1 row.');
+      }
+      test.done();
+    });
+  },
+  /**
+   * Executes last, closes the connection
+   */
+  disconnect : function (test) {
+    test.ok(con.connected, 'Connection not connected to host.');
+    con.close(function () {
+      test.done();
+    });
+  }
+};

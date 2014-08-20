86c7933ef0b41c146b3721a0b8f96f89d2b86707
Jorge Bay <jorge.gondra@datastax.com>
2014-08-20 14:20:20 +0200

    Use HostMap as an associative Array
    - ControlConnection now parses the info and adds the hosts to the map.

--- test/integration/control-connection-tests.js
+++ test/integration/control-connection-tests.js
@@ -1,8 +1,11 @@
+var assert = require('assert');
+
 var ControlConnection = require('../../lib/control-connection.js');
 var RoundRobinPolicy = require('../../lib/policies/load-balancing.js').RoundRobinPolicy;
 
 describe('ControlConnection', function () {
   describe('#init()', function () {
+    //CMM 2
     it('should retrieve local host and peers', function (done) {
       var options = {
         contactPoints: ['127.0.0.1'],
@@ -11,6 +14,12 @@ describe('ControlConnection', function () {
         }};
       var cc = new ControlConnection(options);
       cc.init(function () {
+        assert.equal(cc.hosts.length, 2);
+        cc.hosts.forEach(function (h) {
+          console.log(h);
+          assert.ok(h.datacenter);
+          assert.ok(h.rack);
+        });
         done();
       });
     });

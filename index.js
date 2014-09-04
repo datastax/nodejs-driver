769ec8ebec96df6923e61d26e7e8177ca1c42eed
Jorge Bay <jorge.gondra@datastax.com>
2014-09-04 13:29:09 +0200

    AuthProvider and Authenticator base classes along with PlainTextAuthProvider.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports.Client = require('./lib/client.js');
-module.exports.Connection = require('./lib/connection.js').Connection;
 module.exports.types = require('./lib/types.js');
+//TODO: policies, auth

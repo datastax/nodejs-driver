14bb3bc4b91c8d7f6f4d9bc3f4a2582a76b3685e
Jorge Bay <jorge.gondra@datastax.com>
2014-09-19 15:02:17 +0200

    Murmur3 tokenizer fixes and unit tests

--- test/unit/tokenizer-tests.js
+++ test/unit/tokenizer-tests.js
@@ -19,10 +19,34 @@ describe('Murmur3Tokenizer', function () {
     it('should return expected results', function () {
       var t = new Murmur3Tokenizer();
       assert.strictEqual(t.fmix(Long.fromString('44744441112828')).toString(), '-7224089102552050611');
-//      assert.strictEqual(t.fmix(Long.fromString('9090')).toString(), '-7504869017411790576');
-//      assert.strictEqual(t.fmix(Long.fromString('90913738921')).toString(), '2458123773104054050');
-//      assert.strictEqual(t.fmix(Long.fromString('1')).toString(), '-5451962507482445012');
-//      assert.strictEqual(t.fmix(Long.fromString('-1')).toString(), '7256831767414464289');
+      assert.strictEqual(t.fmix(Long.fromString('9090')).toString(), '-7504869017411790576');
+      assert.strictEqual(t.fmix(Long.fromString('90913738921')).toString(), '2458123773104054050');
+      assert.strictEqual(t.fmix(Long.fromString('1')).toString(), '-5451962507482445012');
+      assert.strictEqual(t.fmix(Long.fromString('-1')).toString(), '7256831767414464289');
+    });
+  });
+  describe('getBlock', function () {
+    it('should return expected results', function () {
+      var t = new Murmur3Tokenizer();
+      assert.strictEqual(t.getBlock([1, 2, 3, 4, 5, 6, 7, 8], 0, 0).toString(), '578437695752307201');
+      assert.strictEqual(t.getBlock([1, -2, 3, 4, 5, 6, 7, -8], 0, 0).toString(), '-574483808854475263');
+      assert.strictEqual(t.getBlock([1, -2, 3, 4, 5, -6, 7, -8], 0, 0).toString(), '-574215528017297919');
+      assert.strictEqual(t.getBlock([100, -2, 3, 4, 5, -6, 7, 122], 0, 0).toString(), '8793271696913333860');
+      assert.strictEqual(t.getBlock([100, -2, 3, 4, -102, -6, 7, 122], 0, 0).toString(), '8793272336863460964');
+    });
+  });
+  describe('hash', function () {
+    it('should hash the according results', function () {
+      var t = new Murmur3Tokenizer();
+      assert.strictEqual(t.hash([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]).toString(), '-5563837382979743776');
+      assert.strictEqual(t.hash([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]).toString(), '-1513403162740402161');
+      assert.strictEqual(t.hash([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]).toString(), '-2824192546314762522');
+      assert.strictEqual(t.hash([0, 1, 2, 3, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]).toString(), '6463632673159404390');
+      assert.strictEqual(t.hash([254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254]).toString(), '-1672437813826982685');
+      assert.strictEqual(t.hash([254, 254, 254, 254]).toString(), '4566408979886474012');
+      assert.strictEqual(t.hash([0, 0, 0, 0]).toString(), '-3485513579396041028');
+      assert.strictEqual(t.hash([0, 1, 127, 127]).toString(), '6573459401642635627');
+      assert.strictEqual(t.hash([226, 231, 226, 231, 226, 231, 1]).toString(), '2222373981930033306');
     });
   });
 });

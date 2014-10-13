f3c8363e1ed6fb7e6765a9b5b3c4a7a521c1bf16
Jorge Bay <jorge.gondra@datastax.com>
2014-10-13 12:08:58 +0200

    jsdoc: Add modules, improve client and query options

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports.Client = require('./lib/client.js');
-module.exports.types = require('./lib/types.js');
-module.exports.errors = require('./lib/errors.js');
-module.exports.policies = require('./lib/policies');
-module.exports.auth = require('./lib/auth');
+exports.Client = require('./lib/client.js');
+exports.types = require('./lib/types.js');
+exports.errors = require('./lib/errors.js');
+exports.policies = require('./lib/policies');
+exports.auth = require('./lib/auth');

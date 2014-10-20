e71e8714aceb7ca50bc5750476e688471e0fd2e8
Jorge Bay <jorge.gondra@datastax.com>
2014-10-20 11:58:39 +0200

    Stress test: insert and select large blobs in parallel

--- test/integration/long/stress-tests.js
+++ test/integration/long/stress-tests.js
@@ -109,6 +109,65 @@ describe('Client', function () {
       }, 500);
     }
   });
+  it('should handle parallel insert and select of large blobs', function (done) {
+    var client = newInstance({policies: { retry: new RetryMultipleTimes(2)}});
+    var keyspace = helper.getRandomName('ks');
+    var table = helper.getRandomName('tbl');
+    var clientInsert = newInstance({keyspace: keyspace, policies: { retry: new RetryMultipleTimes(2)}});
+    var clientSelect = newInstance({keyspace: keyspace, policies: { retry: new RetryMultipleTimes(2)}});
+    var selectQuery = 'SELECT * FROM ' + table + ' LIMIT 10';
+    var insertQuery = util.format('INSERT INTO %s (id, double_sample, blob_sample) VALUES (?, ?, ?)', table);
+    var times = 500;
+    var idArray = [];
+    async.series([
+      helper.ccmHelper.start(3),
+      function createKs(next) {
+        client.execute(helper.createKeyspaceCql(keyspace, 3), [], helper.waitSchema(client, next));
+      },
+      function createTable(next) {
+        client.execute(helper.createTableCql(keyspace + '.' + table), [], helper.waitSchema(client, next));
+      },
+      function testCase(next) {
+        async.parallel([insert, select], next);
+      }
+    ], done);
+    function insert(callback) {
+      var n = 0;
+      async.eachLimit(new Array(times), 100, function (i, next) {
+        i = ++n;
+        var options = {
+          prepare: 1,
+          consistency: types.consistencies.quorum};
+        var buf = new Buffer(i * 1024);
+        buf.write(i + ' dummy values ' + (new Array(100)).join(i.toString()));
+        clientInsert.execute(insertQuery, [types.uuid(), buf.length, buf], options, next);
+      }, callback);
+    }
+    function select(callback) {
+      var resultCount = 0;
+      async.eachLimit(new Array(times*10), 2, function (i, next) {
+        var options = {
+          prepare: 1,
+          consistency: types.consistencies.one,
+          autoPage: true};
+        clientSelect.execute(selectQuery, [], options, function (err, result) {
+          assert.ifError(err);
+          assert.ok(result);
+          if (result.rows) {
+            result.rows.forEach(function (row) {
+              assert.strictEqual(row['blob_sample'].length, row['double_sample']);
+              resultCount++;
+            });
+          }
+          next();
+        });
+      }, function (err) {
+        assert.ifError(err);
+        assert.ok(resultCount > times, 'it should have selected the rows inserted in parallel: ' + resultCount);
+        callback();
+      });
+    }
+  });
 });
 
 /**

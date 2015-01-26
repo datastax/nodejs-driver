745167079ef41414671dc8ba6294c0f062fbacb3
Jorge Bay Gondra <jorge.gondra@datastax.com>
2015-01-26 15:32:04 +0100

    Issue idle request and emit idleRequestError NODEJS-29

--- test/unit/connection-tests.js
+++ test/unit/connection-tests.js
@@ -3,6 +3,7 @@ var async = require('async');
 var util = require('util');
 
 var Connection = require('../../lib/connection.js');
+var requests = require('../../lib/requests');
 var defaultOptions = require('../../lib/client-options.js').defaultOptions();
 var types = require('../../lib/types');
 var utils = require('../../lib/utils.js');
@@ -94,6 +95,79 @@ describe('Connection', function () {
         assert.strictEqual(ioCount, 1);
         done();
       });
+    });
+  });
+  describe('#sendStream()', function () {
+    it('should set the timeout for the idle request', function (done) {
+      var options = utils.extend({logEmitter: helper.noop}, defaultOptions);
+      options.pooling.heartBeatInterval = 60000;
+      var connection = new Connection('address1', 2, options);
+      connection.writeQueue = {
+        push: function (r, cb) {
+          setImmediate(cb);
+          setTimeout(function () {
+            Object
+              .keys(connection.streamHandlers)
+              .map(function (k) {
+                var h = connection.streamHandlers[k];
+                delete connection.streamHandlers[k];
+                return h;
+              })
+              .forEach(function (h) {
+                setImmediate(h.callback);
+              });
+          }, 50);
+        }
+      };
+      assert.ok(!connection.idleTimeout);
+      connection.sendStream({dummy: 'request'}, {}, function () {
+        assert.ok(connection.idleTimeout);
+        connection.close();
+        done();
+      });
+    });
+    it('should not set the timeout for the idle request when heartBeatInterval is 0', function (done) {
+      var options = utils.extend({logEmitter: helper.noop}, defaultOptions);
+      options.pooling.heartBeatInterval = 0;
+      var connection = new Connection('address1', 2, options);
+      connection.writeQueue = {
+        push: function (r, cb) {
+          setImmediate(cb);
+          setTimeout(function () {
+            Object
+              .keys(connection.streamHandlers)
+              .map(function (k) {
+                var h = connection.streamHandlers[k];
+                delete connection.streamHandlers[k];
+                return h;
+              })
+              .forEach(function (h) {
+                setImmediate(h.callback);
+              });
+          }, 50);
+        }
+      };
+      assert.ok(!connection.idleTimeout);
+      connection.sendStream({dummy: 'request'}, {}, function () {
+        assert.ok(!connection.idleTimeout);
+        connection.close();
+        done();
+      });
+    });
+  });
+  describe('#idleTimeoutHandler()', function () {
+    it('should emit idleRequestError if there was an error while executing the request', function (done) {
+      var options = utils.extend({logEmitter: helper.noop}, defaultOptions);
+      var connection = new Connection('address1', 2, options);
+      connection.sendStream = function (req, options, cb) {
+        helper.assertInstanceOf(req, requests.QueryRequest);
+        setImmediate(function () { cb (new Error('Dummy'))});
+      };
+      connection.on('idleRequestError', function (err) {
+        helper.assertInstanceOf(err, Error);
+        done();
+      });
+      connection.idleTimeoutHandler();
     });
   });
 });

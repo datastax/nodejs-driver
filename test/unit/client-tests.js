b73cf09d63d96b071625d34a0935ee291960fb6d
Jorge Bay Gondra <jorge.gondra@datastax.com>
2015-06-26 15:41:49 +0200

    Option to warmup pool on connect NODEJS-138
    - connect() callbacks in error after shutdown NODEJS-111

--- test/unit/client-tests.js
+++ test/unit/client-tests.js
@@ -80,6 +80,31 @@ describe('Client', function () {
         done();
       });
     });
+    it('should fail when trying to connect after shutdown', function (done) {
+      var Client = require('../../lib/client');
+      var options = utils.extend({
+        contactPoints: helper.baseOptions.contactPoints,
+        policies: {
+          loadBalancing: new policies.loadBalancing.RoundRobinPolicy()
+        }
+      });
+      var client = new Client(options);
+      client.controlConnection = {
+        init: helper.callbackNoop,
+        hosts: new HostMap()
+      };
+      async.series([
+        client.connect.bind(client),
+        client.shutdown.bind(client)
+      ], function (err) {
+        assert.ifError(err);
+        client.connect(function (err) {
+          helper.assertInstanceOf(err, errors.NoHostAvailableError);
+          assert.ok(err.message.indexOf('after shutdown') >= 0, 'Error message does not contain occurrence: ' + err.message);
+          done();
+        });
+      });
+    });
   });
   describe('#_getPrepared()', function () {
     var Client = rewire('../../lib/client.js');

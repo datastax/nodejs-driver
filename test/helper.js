856178adf7ce44881898a10e03f05d9d61105d39
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2016-01-14 17:12:17 +0100

    Add test helper with ccm integration

---
+++ test/helper.js
@@ -0,0 +1,265 @@
+var async = require('async');
+var util = require('util');
+var path = require('path');
+
+//noinspection JSUnusedGlobalSymbols
+var helper = {
+  getDseVersion: function() {
+    var version = process.env['TEST_DSE_VERSION'];
+    if (!version) {
+      version = '4.5.2';
+    }
+    return version;
+  },
+  /**
+   * Determines if the current DSE instance version is greater than or equals to the version provided
+   * @param {String} version The version in string format, dot separated.
+   * @returns {Boolean}
+   */
+  isDseGreaterThan: function (version) {
+    var instanceVersion = this.getDseVersion().split('.').map(function (x) { return parseInt(x, 10);});
+    var compareVersion = version.split('.').map(function (x) { return parseInt(x, 10) || 0;});
+    for (var i = 0; i < compareVersion.length; i++) {
+      var compare = compareVersion[i] || 0;
+      if (instanceVersion[i] > compare) {
+        //is greater
+        return true;
+      }
+      else if (instanceVersion[i] < compare) {
+        //is smaller
+        return false;
+      }
+    }
+    //are equal
+    return true;
+  },
+  queries: {
+    basic: "SELECT key FROM system.local",
+    basicNoResults: "SELECT key from system.local WHERE key = 'not_existent'"
+  },
+  /**
+   * Version dependent it() method for mocha test case
+   * @param {String} testVersion Minimum version of Cassandra needed for this test
+   * @param {String} testCase Test case name
+   * @param {Function} func
+   */
+  vit: function (testVersion, testCase, func) {
+    executeIfVersion(testVersion, it, [testCase, func]);
+  },
+
+  /**
+   * Version dependent describe() method for mocha test case
+   * @param {String} testVersion Minimum version of Cassandra needed for this test
+   * @param {String} title Title of the describe section.
+   * @param {Function} func
+   */
+  vdescribe: function (testVersion, title, func) {
+    executeIfVersion(testVersion, describe, [title, func]);
+  },
+  baseOptions: {
+    contactPoints: ['127.0.0.1']
+  },
+  /**
+   * @returns {Function} A function with a single callback param, applying the fn with parameters
+   */
+  toTask: function (fn, context) {
+    var params = Array.prototype.slice.call(arguments, 2);
+    return (function (next) {
+      params.push(next);
+      fn.apply(context, params);
+    });
+  },
+  /**
+   * Determines if test tracing is enabled
+   */
+  isTracing: function () {
+    return (process.env['TEST_TRACE'] === 'on');
+  },
+  trace: function (format) {
+    if (!helper.isTracing()) {
+      return;
+    }
+    console.log('\t...' + util.format.apply(null, arguments));
+  },
+  wait: function (ms, callback) {
+    if (!ms) {
+      ms = 0;
+    }
+    return (function (err) {
+      if (err) return callback(err);
+      setTimeout(callback, ms);
+    });
+  },
+  ccm: {}
+};
+
+/**
+ * Removes previous and creates a new cluster (create, populate and start)
+ * @param {Number|String} nodeLength number of nodes in the cluster. If multiple dcs, use the notation x:y:z:...
+ * @param {{vnodes: Boolean, yaml: Array}} options
+ * @param {Function} callback
+ */
+helper.ccm.startAll = function (nodeLength, options, callback) {
+  var self = this;
+  options = options || {};
+  var version = helper.getDseVersion();
+  helper.trace('Starting test DSE cluster v%s with %s node(s)', version, nodeLength);
+  async.series([
+    function (next) {
+      //it wont hurt to remove
+      self.exec(['remove'], function () {
+        //ignore error
+        next();
+      });
+    },
+    function (next) {
+      var create = ['create', 'test', '--dse', '-v', version];
+      if (process.env['TEST_DSE_DIR']) {
+        create = ['create', 'test', '--install-dir=' + process.env['TEST_DSE_DIR']];
+        helper.trace('With', create[2]);
+      }
+      if (options.ssl) {
+        create.push('--ssl', self.getPath('ssl'));
+      }
+      self.exec(create, helper.wait(options.sleep, next));
+    },
+    function (next) {
+      if (!options.yaml) {
+        return next();
+      }
+      var i = 0;
+      async.whilst(
+        function condition() {
+          return i < options.yaml.length
+        },
+        function iterator(whilstNext) {
+          self.exec(['updateconf', options.yaml[i++]], whilstNext);
+        },
+        next
+      );
+    },
+    function (next) {
+      var populate = ['populate', '-n', nodeLength.toString()];
+      if (options.vnodes) {
+        populate.push('--vnodes');
+      }
+      self.exec(populate, helper.wait(options.sleep, next));
+    },
+    function (next) {
+      var start = ['start', '--wait-for-binary-proto'];
+      if (util.isArray(options.jvmArgs)) {
+        options.jvmArgs.forEach(function (arg) {
+          start.push('--jvm_arg', arg);
+        }, this);
+        helper.trace('With jvm args', options.jvmArgs);
+      }
+      self.exec(start, helper.wait(options.sleep, next));
+    },
+    self.waitForUp.bind(self)
+  ], function (err) {
+    callback(err);
+  });
+};
+
+helper.ccm.exec = function (params, callback) {
+  this.spawn('ccm', params, callback);
+};
+
+helper.ccm.spawn = function (processName, params, callback) {
+  if (!callback) {
+    callback = function () {};
+  }
+  params = params || [];
+  var originalProcessName = processName;
+  var spawn = require('child_process').spawn;
+  if (process.platform.indexOf('win') === 0) {
+    params = ['/c', processName].concat(params);
+    processName = 'cmd.exe';
+  }
+  var p = spawn(processName, params);
+  var stdoutArray= [];
+  var stderrArray= [];
+  var closing = 0;
+  p.stdout.setEncoding('utf8');
+  p.stderr.setEncoding('utf8');
+  p.stdout.on('data', function (data) {
+    stdoutArray.push(data);
+  });
+
+  p.stderr.on('data', function (data) {
+    stderrArray.push(data);
+  });
+
+  p.on('close', function (code) {
+    if (closing++ > 0) {
+      //avoid calling multiple times
+      return;
+    }
+    var info = {code: code, stdout: stdoutArray, stderr: stderrArray};
+    var err = null;
+    if (code !== 0) {
+      err = new Error(
+        'Error executing ' + originalProcessName + ':\n' +
+        info.stderr.join('\n') +
+        info.stdout.join('\n')
+      );
+      err.info = info;
+    }
+    callback(err, info);
+  });
+};
+
+helper.ccm.remove = function (callback) {
+  this.exec(['remove'], callback);
+};
+
+/**
+ * Reads the logs to see if the cql protocol is up
+ * @param callback
+ */
+helper.ccm.waitForUp = function (callback) {
+  var started = false;
+  var retryCount = 0;
+  var self = this;
+  async.whilst(function () {
+    return !started && retryCount < 10;
+  }, function iterator (next) {
+    self.exec(['node1', 'showlog'], function (err, info) {
+      if (err) return next(err);
+      var regex = /Starting listening for CQL clients/mi;
+      started = regex.test(info.stdout.join(''));
+      retryCount++;
+      if (!started) {
+        //wait 1 sec between retries
+        return setTimeout(next, 1000);
+      }
+      return next();
+    });
+  }, callback);
+};
+
+/**
+ * Gets the path of the ccm
+ * @param subPath
+ */
+helper.ccm.getPath = function (subPath) {
+  var ccmPath = process.env.CCM_PATH;
+  if (!ccmPath) {
+    ccmPath = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
+    ccmPath = path.join(ccmPath, 'workspace/tools/ccm');
+  }
+  return path.join(ccmPath, subPath);
+};
+/**
+ * Conditionally executes func if testVersion is <= the current cassandra version.
+ * @param {String} testVersion Minimum version of Cassandra needed.
+ * @param {Function} func The function to conditionally execute.
+ * @param {Array} args the arguments to apply to the function.
+ */
+function executeIfVersion (testVersion, func, args) {
+  if (helper.isDseGreaterThan(testVersion)) {
+    func.apply(this, args);
+  }
+}
+
+module.exports = helper;

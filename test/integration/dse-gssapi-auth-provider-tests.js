a96328f267ca32d6462151fe095edd9c2b41d11c
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2016-01-27 11:33:30 +0100

    Integration test for DSE 5 GSSAPI auth

--- test/integration/dse-gssapi-auth-provider-tests.js
+++ test/integration/dse-gssapi-auth-provider-tests.js
@@ -27,4 +27,17 @@ describe('DseGssapiAuthProvider', function () {
       helper.connectAndQuery(new cassandra.Client(clientOptions), done);
     });
   });
+  vit('5.0', 'should authenticate against DSE v5.x instance', function (done) {
+    var testClusterOptions = {
+      yaml: ['authenticator: com.datastax.bdp.cassandra.auth.DseAuthenticator'],
+      dseYaml: ads.getDseKerberosOptions(),
+      jvmArgs: ['-Dcassandra.superuser_setup_delay_ms=0', '-Djava.security.krb5.conf=' + ads.getKrb5ConfigPath()]
+    };
+    helper.ccm.startAll(1, testClusterOptions, function (err) {
+      assert.ifError(err);
+      var authProvider = new DseGssAuthProvider();
+      var clientOptions = helper.getOptions({ authProvider: authProvider });
+      helper.connectAndQuery(new cassandra.Client(clientOptions), done);
+    });
+  });
 });

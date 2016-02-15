ef4eb27029571cbbf9cec16e5da2d6ba6ceae315
Andrew Tolbert <andrew.tolbert@datastax.com>
2016-02-14 22:15:25 -0600

    Add collection and partition key tests for geospatial types.

--- test/integration/point-tests.js
+++ test/integration/point-tests.js
@@ -4,10 +4,12 @@ var util = require('util');
 var async = require('async');
 var helper = require('../helper');
 var cassandra = require('cassandra-driver');
-var DsePlainTextAuthProvider = require('../../lib/auth/dse-plain-text-auth-provider');
 var vdescribe = helper.vdescribe;
 var encoderExtensions = require('../../lib/encoder-extensions');
 encoderExtensions.register(cassandra.Encoder);
+var types = cassandra.types;
+var Uuid = types.Uuid;
+var Tuple = types.Tuple;
 var Point = require('../../lib/types/point');
 
 vdescribe('5.0', 'Point', function () {
@@ -26,7 +28,9 @@ vdescribe('5.0', 'Point', function () {
           "CREATE TABLE points (id text, value 'PointType', PRIMARY KEY (id))",
           "INSERT INTO points (id, value) VALUES ('POINT(0 0)', 'POINT(0 0)')",
           "INSERT INTO points (id, value) VALUES ('POINT(2 4)', 'POINT(2 4)')",
-          "INSERT INTO points (id, value) VALUES ('POINT(-1.2 -100)', 'POINT(-1.2 -100)')"
+          "INSERT INTO points (id, value) VALUES ('POINT(-1.2 -100)', 'POINT(-1.2 -100)')",
+          "CREATE TABLE keyed (id 'PointType', value text, PRIMARY KEY (id))",
+          "INSERT INTO keyed (id, value) VALUES ('POINT(1 0)', 'hello')"
         ];
         async.eachSeries(queries, function (q, eachNext) {
           client.execute(q, eachNext);
@@ -60,67 +64,183 @@ vdescribe('5.0', 'Point', function () {
       client.shutdown.bind(client)
     ], done);
   });
-  it('should encode points for simple queries', function (done) {
-    var client = new cassandra.Client(helper.getOptions());
-    async.series([
-      client.connect.bind(client),
-      function test(next) {
-        var values = [
-          new Point(1.2, 3.9),
-          new Point(-1.2, 1.9),
-          new Point(0.21222, 3122.9)
-        ];
-        var insertQuery = 'INSERT INTO ks1.points (id, value) VALUES (?, ?)';
-        var selectQuery = 'SELECT toJSON(value) as json_value FROM ks1.points WHERE id = ?';
-        async.each(values, function (p, eachNext) {
-          var id = util.format('simple[%d,%d]', p.x, p.y);
-          client.execute(insertQuery, [id, p], { prepare: false }, function (err) {
-            assert.ifError(err);
-            client.execute(selectQuery, [id], function (err, result) {
+  [0, 1].forEach(function (prepare) {
+    var name = prepare ? 'prepared' : 'simple';
+    it(util.format('should encode points for %s queries', name), function (done) {
+      var client = new cassandra.Client(helper.getOptions());
+      async.series([
+        client.connect.bind(client),
+        function test(next) {
+          var values = [
+            new Point(1.2, 3.9),
+            new Point(-1.2, 1.9),
+            new Point(0.21222, 3122.9)
+          ];
+          var insertQuery = 'INSERT INTO ks1.points (id, value) VALUES (?, ?)';
+          var selectQuery = 'SELECT toJSON(value) as json_value FROM ks1.points WHERE id = ?';
+          var counter = 0;
+          async.each(values, function (p, eachNext) {
+            var id = util.format('%s-%d', name, ++counter);
+            client.execute(insertQuery, [id, p], { prepare: prepare}, function (err) {
               assert.ifError(err);
-              var row = result.first();
-              assert.ok(row);
-              //use json value to avoid decoding client side in this test
-              var value = JSON.parse(row['json_value']);
-              assert.deepEqual(value.coordinates, [p.x, p.y]);
-              eachNext();
+              client.execute(selectQuery, [id], function (err, result) {
+                assert.ifError(err);
+                var row = result.first();
+                assert.ok(row);
+                //use json value to avoid decoding client side in this test
+                var value = JSON.parse(row['json_value']);
+                assert.deepEqual(value.coordinates, [p.x, p.y]);
+                eachNext();
+              });
             });
+          }, next);
+        },
+        client.shutdown.bind(client)
+      ], done);
+    });
+    it(util.format('should be able to retrieve data where point is partition key for %s queries', name), function (done) {
+      var client = new cassandra.Client(helper.getOptions());
+      var id = new Point(1, 0);
+      async.series([
+        client.connect.bind(client),
+        function (next) {
+          var selectQuery = 'SELECT value FROM ks1.keyed WHERE id = ?';
+          client.execute(selectQuery, [id], function (err, result) {
+            assert.ifError(err);
+            var row = result.first();
+            assert.ok(row);
+            assert.strictEqual(row['value'], 'hello');
+            next();
           });
-        }, next);
-      },
-      client.shutdown.bind(client)
-    ], done);
+        },
+        client.shutdown.bind(client)
+      ], done);
+    });
   });
-  it('should encode points for prepared queries', function (done) {
-    var client = new cassandra.Client(helper.getOptions());
-    async.series([
-      client.connect.bind(client),
-      function test(next) {
-        var values = [
-          new Point(-1.2, -3.9),
-          new Point(1.2, -1.9),
-          new Point(10.21222, 1622.9)
-        ];
-        var insertQuery = 'INSERT INTO ks1.points (id, value) VALUES (?, ?)';
-        var selectQuery = 'SELECT toJSON(value) as json_value FROM ks1.points WHERE id = ?';
-        async.each(values, function (p, eachNext) {
-          var id = util.format('simple[%d,%d]', p.x, p.y);
-          client.execute(insertQuery, [id, p], { prepare: true }, function (err) {
-            assert.ifError(err);
-            client.execute(selectQuery, [id], function (err, result) {
+  describe('with collections, tuples and udts', function () {
+    var point = new Point(0, 0);
+    var point2 = new Point(1, 1);
+    before(function (done) {
+      var client = new cassandra.Client(helper.getOptions());
+      async.series([
+        client.connect.bind(client),
+        function createAll(next) {
+          var queries = [
+            "use ks1",
+            "CREATE TYPE pointt (f text, v 'PointType')",
+            "CREATE TABLE tbl_udts (id uuid PRIMARY KEY, udt_col frozen<pointt>)",
+            "CREATE TABLE tbl_tuple (id uuid PRIMARY KEY, tuple_col tuple<int, 'PointType'>)",
+            "CREATE TABLE tbl_list (id uuid PRIMARY KEY, list_col list<'PointType'>)",
+            "CREATE TABLE tbl_set (id uuid PRIMARY KEY, set_col set<'PointType'>)",
+            "CREATE TABLE tbl_map (id uuid PRIMARY KEY, map_col map<text, 'PointType'>)"
+          ];
+          async.eachSeries(queries, function (q, eachNext) {
+            client.execute(q, eachNext);
+          }, next);
+        },
+        client.shutdown.bind(client)
+      ], done);
+    });
+    [0, 1].forEach(function (prepare) {
+      var name = prepare ? 'prepared' : 'simple';
+      it(util.format('should create and retrieve points in a udt for %s queries', name), function (done) {
+        var client = new cassandra.Client(helper.getOptions());
+        var insertQuery = 'INSERT INTO ks1.tbl_udts (id, udt_col) values (?, ?)';
+        var selectQuery = 'SELECT udt_col FROM ks1.tbl_udts WHERE id = ?';
+        var id = Uuid.random();
+        var udt = { f: 'hello', v: point};
+
+        async.series([
+          client.connect.bind(client),
+          function (next) {
+            client.execute(insertQuery, [id, udt], {prepare: prepare, hints: [null, 'udt<ks1.pointt>']}, function (err) {
               assert.ifError(err);
-              var row = result.first();
-              assert.ok(row);
-              //use json value to avoid decoding client side in this test
-              var value = JSON.parse(row['json_value']);
-              assert.deepEqual(value.coordinates, [p.x, p.y]);
-              eachNext();
+              client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                assert.ifError(err);
+                var row = result.first();
+                assert.ok(row);
+                assert.deepEqual(row['udt_col'], udt);
+                next();
+              });
             });
-          });
-        }, next);
-      },
-      client.shutdown.bind(client)
-    ], done);
+          },
+          client.shutdown.bind(client)
+        ], done);
+      });
+      it(util.format('should create and retrieve points in a tuple for %s queries', name), function (done) {
+        var client = new cassandra.Client(helper.getOptions());
+        var insertQuery = 'INSERT INTO ks1.tbl_tuple (id, tuple_col) values (?, ?)';
+        var selectQuery = 'SELECT tuple_col FROM ks1.tbl_tuple WHERE id = ?';
+        var id = Uuid.random();
+        var tuple = new Tuple(0, point);
+
+        async.series([
+          client.connect.bind(client),
+          function (next) {
+            client.execute(insertQuery, [id, tuple], {prepare: prepare, hints: [null, "tuple<int,'PointType'>"]}, function (err) {
+              assert.ifError(err);
+              client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                assert.ifError(err);
+                var row = result.first();
+                assert.ok(row);
+                assert.deepEqual(row['tuple_col'], tuple);
+                next();
+              });
+            });
+          },
+          client.shutdown.bind(client)
+        ], done);
+      });
+      ['list', 'set'].forEach(function (colType) {
+        it(util.format('should create and retrieve points in a %s for %s queries', colType, name), function (done) {
+          var client = new cassandra.Client(helper.getOptions());
+          var insertQuery = util.format('INSERT INTO ks1.tbl_%s (id, %s_col) values (?, ?)', colType, colType);
+          var selectQuery = util.format('SELECT %s_col FROM ks1.tbl_%s WHERE id = ?', colType, colType);
+          var id = Uuid.random();
+          var data = [point, point2];
+          async.series([
+            client.connect.bind(client),
+            function (next) {
+              client.execute(insertQuery, [id, data], {prepare: prepare}, function (err) {
+                assert.ifError(err);
+                client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                  assert.ifError(err);
+                  var row = result.first();
+                  assert.ok(row);
+                  assert.deepEqual(row[util.format('%s_col', colType)], data);
+                  next();
+                });
+              });
+            },
+            client.shutdown.bind(client)
+          ], done);
+        });
+      });
+      it(util.format('should create and retrieve points in a map for %s queries', name), function (done) {
+        var client = new cassandra.Client(helper.getOptions());
+        var insertQuery = 'INSERT INTO ks1.tbl_map (id, map_col) values (?, ?)';
+        var selectQuery = 'SELECT map_col FROM ks1.tbl_map WHERE id = ?';
+        var id = Uuid.random();
+        var map = { point : point };
+
+        async.series([
+          client.connect.bind(client),
+          function (next) {
+            client.execute(insertQuery, [id, map], {prepare: prepare, hints: [null, types.dataTypes.map]}, function (err) {
+              assert.ifError(err);
+              client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                assert.ifError(err);
+                var row = result.first();
+                assert.ok(row);
+                assert.deepEqual(row['map_col'], map);
+                next();
+              });
+            });
+          },
+          client.shutdown.bind(client)
+        ], done);
+      });
+    });
   });
   after(helper.ccm.remove.bind(helper.ccm));
 });
--- test/integration/polygon-tests.js
+++ test/integration/polygon-tests.js
@@ -4,11 +4,13 @@ var util = require('util');
 var async = require('async');
 var helper = require('../helper');
 var cassandra = require('cassandra-driver');
-var DsePlainTextAuthProvider = require('../../lib/auth/dse-plain-text-auth-provider');
 var vdescribe = helper.vdescribe;
 var encoderExtensions = require('../../lib/encoder-extensions');
 var Point = require('../../lib/types/point');
 var Polygon = require('../../lib/types/polygon');
+var types = cassandra.types;
+var Uuid = types.Uuid;
+var Tuple = types.Tuple;
 encoderExtensions.register(cassandra.Encoder);
 
 vdescribe('5.0', 'Polygon', function () {
@@ -27,7 +29,9 @@ vdescribe('5.0', 'Polygon', function () {
           "CREATE TABLE polygons (id text, value 'PolygonType', PRIMARY KEY (id))",
           "INSERT INTO polygons (id, value) VALUES ('sample1', 'POLYGON ((1 3, 3 1, 3 6, 1 3))')",
           "INSERT INTO polygons (id, value) VALUES ('sample2', 'POLYGON((0 10, 10 0, 10 10, 0 10), (6 7,3 9,9 9,6 7))')",
-          "INSERT INTO polygons (id, value) VALUES ('sample3', 'POLYGON EMPTY')"
+          "INSERT INTO polygons (id, value) VALUES ('sample3', 'POLYGON EMPTY')",
+          "CREATE TABLE keyed (id 'PolygonType', value text, PRIMARY KEY (id))",
+          "INSERT INTO keyed (id, value) VALUES ('POLYGON ((1 3, 3 1, 3 6, 1 3))', 'hello')"
         ];
         async.eachSeries(queries, function (q, eachNext) {
           client.execute(q, eachNext);
@@ -107,6 +111,149 @@ vdescribe('5.0', 'Polygon', function () {
         },
         client.shutdown.bind(client)
       ], done);
+    });
+    it(util.format('should be able to retrieve data where polygon is partition key for %s queries', name), function (done) {
+      var client = new cassandra.Client(helper.getOptions());
+      var id = new Polygon([new Point(1, 3), new Point(3, 1), new Point(3, 6), new Point(1, 3)]);
+      async.series([
+        client.connect.bind(client),
+        function (next) {
+          var selectQuery = 'SELECT value FROM ks1.keyed WHERE id = ?';
+          client.execute(selectQuery, [id], function (err, result) {
+            assert.ifError(err);
+            var row = result.first();
+            assert.ok(row);
+            assert.strictEqual(row['value'], 'hello');
+            next();
+          });
+        },
+        client.shutdown.bind(client)
+      ], done);
+    });
+  });
+  describe('with collections, tuples and udts', function () {
+    var polygon = new Polygon();
+    var polygon2 = new Polygon([new Point(1, 3), new Point(3, 1), new Point(3, 6), new Point(1, 3)]);
+    before(function (done) {
+      var client = new cassandra.Client(helper.getOptions());
+      async.series([
+        client.connect.bind(client),
+        function createAll(next) {
+          var queries = [
+            "use ks1",
+            "CREATE TYPE polygont (f text, v 'PolygonType')",
+            "CREATE TABLE tbl_udts (id uuid PRIMARY KEY, udt_col frozen<polygont>)",
+            "CREATE TABLE tbl_tuple (id uuid PRIMARY KEY, tuple_col tuple<int, 'PolygonType'>)",
+            "CREATE TABLE tbl_list (id uuid PRIMARY KEY, list_col list<'PolygonType'>)",
+            "CREATE TABLE tbl_set (id uuid PRIMARY KEY, set_col set<'PolygonType'>)",
+            "CREATE TABLE tbl_map (id uuid PRIMARY KEY, map_col map<text, 'PolygonType'>)"
+          ];
+          async.eachSeries(queries, function (q, eachNext) {
+            client.execute(q, eachNext);
+          }, next);
+        },
+        client.shutdown.bind(client)
+      ], done);
+    });
+    [0, 1].forEach(function (prepare) {
+      var name = prepare ? 'prepared' : 'simple';
+      it(util.format('should create and retrieve polygons in a udt for %s queries', name), function (done) {
+        var client = new cassandra.Client(helper.getOptions());
+        var insertQuery = 'INSERT INTO ks1.tbl_udts (id, udt_col) values (?, ?)';
+        var selectQuery = 'SELECT udt_col FROM ks1.tbl_udts WHERE id = ?';
+        var id = Uuid.random();
+        var udt = { f: 'hello', v: polygon};
+
+        async.series([
+          client.connect.bind(client),
+          function (next) {
+            client.execute(insertQuery, [id, udt], {prepare: prepare, hints: [null, 'udt<ks1.polygont>']}, function (err) {
+              assert.ifError(err);
+              client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                assert.ifError(err);
+                var row = result.first();
+                assert.ok(row);
+                assert.deepEqual(row['udt_col'], udt);
+                next();
+              });
+            });
+          },
+          client.shutdown.bind(client)
+        ], done);
+      });
+      it(util.format('should create and retrieve polygons in a tuple for %s queries', name), function (done) {
+        var client = new cassandra.Client(helper.getOptions());
+        var insertQuery = 'INSERT INTO ks1.tbl_tuple (id, tuple_col) values (?, ?)';
+        var selectQuery = 'SELECT tuple_col FROM ks1.tbl_tuple WHERE id = ?';
+        var id = Uuid.random();
+        var tuple = new Tuple(0, polygon);
+
+        async.series([
+          client.connect.bind(client),
+          function (next) {
+            client.execute(insertQuery, [id, tuple], {prepare: prepare, hints: [null, "tuple<int,'PolygonType'>"]}, function (err) {
+              assert.ifError(err);
+              client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                assert.ifError(err);
+                var row = result.first();
+                assert.ok(row);
+                assert.deepEqual(row['tuple_col'], tuple);
+                next();
+              });
+            });
+          },
+          client.shutdown.bind(client)
+        ], done);
+      });
+      ['list', 'set'].forEach(function (colType) {
+        it(util.format('should create and retrieve polygons in a %s for %s queries', colType, name), function (done) {
+          var client = new cassandra.Client(helper.getOptions());
+          var insertQuery = util.format('INSERT INTO ks1.tbl_%s (id, %s_col) values (?, ?)', colType, colType);
+          var selectQuery = util.format('SELECT %s_col FROM ks1.tbl_%s WHERE id = ?', colType, colType);
+          var id = Uuid.random();
+          var data = [polygon, polygon2];
+          async.series([
+            client.connect.bind(client),
+            function (next) {
+              client.execute(insertQuery, [id, data], {prepare: prepare}, function (err) {
+                assert.ifError(err);
+                client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                  assert.ifError(err);
+                  var row = result.first();
+                  assert.ok(row);
+                  assert.deepEqual(row[util.format('%s_col', colType)], data);
+                  next();
+                });
+              });
+            },
+            client.shutdown.bind(client)
+          ], done);
+        });
+      });
+      it(util.format('should create and retrieve polygons in a map for %s queries', name), function (done) {
+        var client = new cassandra.Client(helper.getOptions());
+        var insertQuery = 'INSERT INTO ks1.tbl_map (id, map_col) values (?, ?)';
+        var selectQuery = 'SELECT map_col FROM ks1.tbl_map WHERE id = ?';
+        var id = Uuid.random();
+        var map = { polygon : polygon };
+
+        async.series([
+          client.connect.bind(client),
+          function (next) {
+            client.execute(insertQuery, [id, map], {prepare: prepare, hints: [null, types.dataTypes.map]}, function (err) {
+              assert.ifError(err);
+              client.execute(selectQuery, [id], {prepare: prepare}, function (err, result) {
+                assert.ifError(err);
+                var row = result.first();
+                assert.ok(row);
+                assert.deepEqual(row['map_col'], map);
+                next();
+              });
+            });
+          },
+          client.shutdown.bind(client)
+        ], done);
+      });
     });
   });
   after(helper.ccm.remove.bind(helper.ccm));

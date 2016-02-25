68bdac3dc4e67304f09582d7a9ab72767bf1871d
Andrew Tolbert <andrew.tolbert@datastax.com>
2016-02-25 12:23:43 -0600

    Reduce parallelism in tests.
    Needed to reduce sensitivity in windows environment.
    
    done(err) instead of assert.ifError in callback in parallel batch test.

--- test/integration/short/client-batch-tests.js
+++ test/integration/short/client-batch-tests.js
@@ -371,7 +371,7 @@ describe('Client', function () {
       var client = newInstance();
       utils.parallel([
         function (next) {
-          utils.timesLimit(1000, 100, function (n, eachNext) {
+          utils.timesLimit(1000, 25, function (n, eachNext) {
             var queries = [{
               query: query1Table1,
               params: [id1Tbl1, types.timeuuid(), types.BigDecimal.fromNumber(new Date().getTime())]
@@ -383,7 +383,7 @@ describe('Client', function () {
           }, next);
         },
         function (next) {
-          utils.timesLimit(1000, 100, function (n, eachNext) {
+          utils.timesLimit(1000, 25, function (n, eachNext) {
             var queries = [{
               query: query2Table1,
               params: [id2Tbl1, types.timeuuid(), types.BigDecimal.fromNumber(new Date().getTime())]
@@ -395,7 +395,9 @@ describe('Client', function () {
           }, next);
         }
       ], function (err) {
-        assert.ifError(err);
+        if(err) {
+          return done(err);
+        }
         //verify results in both tables
         var q = 'SELECT * FROM %s where id IN (%s, %s)';
         utils.series([

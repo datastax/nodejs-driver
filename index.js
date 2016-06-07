1d706b7ed0331a210b57910187801bc87f61dfb1
Andrew Tolbert <andrew.tolbert@datastax.com>
2016-06-07 13:35:09 -0500

    Expose package version

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ module.exports = {
   errors: cassandra.errors,
   metadata: cassandra.metadata,
   policies: cassandra.policies,
-  types: cassandra.types
+  types: cassandra.types,
+  version: require('./package.json').version
 };

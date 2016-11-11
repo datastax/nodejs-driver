ba7fc758899d46c90a40e3060f4bc4edbbd43e0b
Andrew Tolbert <andrew.tolbert@datastax.com>
2016-11-11 10:58:09 -0600

    Update TokenAwarePolicy tests to account for random replica order
    Also refactor tests and add test cases for validating the behavior when
    no keyspace is provided and the closest replica is or isn't in the local
    DC.

--- test/integration/long/load-balancing-tests.js
+++ test/integration/long/load-balancing-tests.js
@@ -48,123 +48,171 @@ describe('DCAwareRoundRobinPolicy', function () {
 });
 describe('TokenAwarePolicy', function () {
   this.timeout(120000);
-  it('should use primary replica according to Murmur multiple dc', function (done) {
+  describe('with a 3:3 node topology', function() {
     var keyspace = 'ks1';
     var table = 'table1';
-    utils.series([
-      helper.ccmHelper.start('3:3'),
-      function createKs(next) {
-        var client = new Client(helper.baseOptions);
-        var createQuery = "CREATE KEYSPACE %s WITH replication = {'class': 'NetworkTopologyStrategy', 'dc1' : %d, 'dc2' : %d}";
-        client.execute(util.format(createQuery, keyspace, 3, 3), helper.waitSchema(client, next));
-      },
-      function createTable(next) {
-        var client = new Client(helper.baseOptions);
-        var query = util.format('CREATE TABLE %s.%s (id int primary key, name int)', keyspace, table);
-        client.execute(query, helper.waitSchema(client, next));
-      },
-      function testCase(next) {
-        //Pre-calculated based on Murmur
-        //This test can be improved using query tracing, consistency all and checking hops
-        var expectedPartition = {
-          '1': '2',
-          '2': '2',
-          '3': '1',
-          '4': '3',
-          '5': '2',
-          '6': '3',
-          '7': '3',
-          '8': '2',
-          '9': '1',
-          '10': '2'
-        };
-        var client = new Client({
-          policies: { loadBalancing: new TokenAwarePolicy(new DCAwareRoundRobinPolicy())},
-          keyspace: keyspace,
-          contactPoints: helper.baseOptions.contactPoints
-        });
-        utils.times(100, function (n, timesNext) {
-          var id = (n % 10) + 1;
-          var query = util.format('INSERT INTO %s (id, name) VALUES (%s, %s)', table, id, id);
-          client.execute(query, null, {routingKey: new Buffer([0, 0, 0, id])}, function (err, result) {
-            assert.ifError(err);
-            //for murmur id = 1, it go to replica 2
-            var address = result.info.queriedHost;
-            assert.strictEqual(helper.lastOctetOf(address), expectedPartition[id.toString()]);
-            timesNext();
-          });
-        }, next);
-      },
-      helper.ccmHelper.remove
-    ], done);
-  });
-  it('should yield local replicas plus childPolicy plus remote replicas', function (done) {
-    var keyspace1 = 'ks1';
-    var keyspace2 = 'ks2';
-    var createQuery = "CREATE KEYSPACE %s WITH replication = {'class': 'NetworkTopologyStrategy', 'dc1' : %d, 'dc2' : %d}";var client = new Client({
+    var client = new Client({
       policies: { loadBalancing: new TokenAwarePolicy(new DCAwareRoundRobinPolicy())},
+      keyspace: keyspace,
       contactPoints: helper.baseOptions.contactPoints
     });
-    /** @type {LoadBalancingPolicy} */
-    var policy = client.options.policies.loadBalancing;
-    var localDc = 'dc1';
-    utils.series([
-      helper.ccmHelper.start('4:4'),
-      function createKs1(next) {
-        var localClient = new Client(helper.baseOptions);
-        localClient.execute(util.format(createQuery, keyspace1, 3, 3), helper.waitSchema(localClient, next));
-      },
-      function createKs1(next) {
-        var localClient = new Client(helper.baseOptions);
-        localClient.execute(util.format(createQuery, keyspace2, 1, 1), helper.waitSchema(localClient, next));
-      },
-      client.connect.bind(client),
-      function testCase1(next) {
-        utils.times(20, function (n, timesNext) {
-          //keyspace 1
-          policy.newQueryPlan(keyspace1, {routingKey: new Buffer([0, 0, 0, 1])}, function (err, iterator) {
-            var hosts = helper.iteratorToArray(iterator);
-            //6 replicas plus an additional local node
-            assert.ok(hosts.length, 7);
-            assert.strictEqual(hosts[0].datacenter, localDc);
-            assert.strictEqual(hosts[1].datacenter, localDc);
-            assert.strictEqual(hosts[2].datacenter, localDc);
-            assert.strictEqual(hosts[3].datacenter, localDc);
-            timesNext();
+
+    before(function (done) {
+      var localClient = new Client(helper.baseOptions);
+      utils.series([
+        helper.ccmHelper.start('3:3'),
+        localClient.connect.bind(localClient),
+        function createKs(next) {
+          var createQuery = "CREATE KEYSPACE %s WITH replication = {'class': 'NetworkTopologyStrategy', 'dc1' : %d, 'dc2' : %d}";
+          localClient.execute(util.format(createQuery, keyspace, 1, 1), helper.waitSchema(localClient, next));
+        },
+        function createTable(next) {
+          var query = util.format('CREATE TABLE %s.%s (id int primary key, name int)', keyspace, table);
+          localClient.execute(query, helper.waitSchema(localClient, next));
+        },
+        localClient.shutdown.bind(localClient)
+      ], done);
+    });
+    after(function (done) {
+      utils.series([
+        helper.ccmHelper.remove,
+        client.shutdown.bind(client)
+      ], done);
+    });
+    it('should use primary replica according to murmur multiple dc', function (done) {
+      //Pre-calculated based on Murmur
+      //This test can be improved using query tracing, consistency all and checking hops
+      var expectedPartition = {
+        '1': '2',
+        '2': '2',
+        '3': '1',
+        '4': '3',
+        '5': '2',
+        '6': '3',
+        '7': '3',
+        '8': '2',
+        '9': '1',
+        '10': '2'
+      };
+      utils.times(100, function (n, timesNext) {
+        var id = (n % 10) + 1;
+        var query = util.format('INSERT INTO %s (id, name) VALUES (%s, %s)', table, id, id);
+        client.execute(query, null, {routingKey: new Buffer([0, 0, 0, id])}, function (err, result) {
+          assert.ifError(err);
+          //for murmur id = 1, it go to replica 2
+          var address = result.info.queriedHost;
+          assert.strictEqual(helper.lastOctetOf(address), expectedPartition[id.toString()]);
+          timesNext();
+        });
+      }, done);
+    });
+  });
+  describe('with a 4:4 node topology', function() {
+    var keyspace1 = 'ks1';
+    var keyspace2 = 'ks2';
+    // Resolves to token -4069959284402364209 which should have primary replica of 3 and 7 with 3 being the closest replica.
+    var routingKey = new Buffer([0, 0, 0, 1]);
+
+    var client_dc2 = new Client({
+      policies: { loadBalancing: new TokenAwarePolicy(new DCAwareRoundRobinPolicy())},
+      contactPoints: ['127.0.0.5'] // choose a host in dc2, for closest replica local selection validation.
+    });
+    var policy_dc2 = client_dc2.options.policies.loadBalancing;
+
+    var client_dc1 = new Client({
+      policies: { loadBalancing: new TokenAwarePolicy(new DCAwareRoundRobinPolicy())},
+      contactPoints: ['127.0.0.1']
+    });
+    var policy_dc1 = client_dc1.options.policies.loadBalancing;
+    var localDc = 'dc2';
+
+    before(function (done) {
+      var createQuery = "CREATE KEYSPACE %s WITH replication = {'class': 'NetworkTopologyStrategy', 'dc1' : %d, 'dc2' : %d}";
+      /** @type {LoadBalancingPolicy} */
+      utils.series([
+        helper.ccmHelper.start('4:4'),
+        function createKs1(next) {
+          client_dc1.execute(util.format(createQuery, keyspace1, 2, 2), helper.waitSchema(client_dc1, next));
+        },
+        function createKs2(next) {
+          client_dc1.execute(util.format(createQuery, keyspace2, 1, 1), helper.waitSchema(client_dc1, next));
+        },
+        client_dc2.connect.bind(client_dc2)
+      ], done);
+    });
+    after(function (done) {
+      utils.series([
+        helper.ccmHelper.remove,
+        client_dc1.shutdown.bind(client_dc1),
+        client_dc2.shutdown.bind(client_dc2)
+      ], done);
+    });
+
+    it('should yield 2 local replicas first, then 2 remaining local nodes when RF is 2', function (done) {
+      utils.times(20, function (n, timesNext) {
+        //keyspace 1
+        policy_dc2.newQueryPlan(keyspace1, {routingKey: routingKey}, function (err, iterator) {
+          var hosts = helper.iteratorToArray(iterator);
+          // 2 local replicas first, 2 remaining local nodes.
+          assert.ok(hosts.length, 4);
+          hosts.forEach(function(host) {
+            assert.strictEqual(host.datacenter, localDc);
           });
-        }, next);
-      },
-      function testCase2(next) {
-        utils.times(20, function (n, timesNext) {
-          //keyspace 2
-          policy.newQueryPlan(keyspace2, {routingKey: new Buffer([0, 0, 0, 1])}, function (err, iterator) {
-            var hosts = helper.iteratorToArray(iterator);
-            //2 replicas plus 3 additional local nodes
-            assert.ok(hosts.length, 5);
-            assert.strictEqual(hosts[0].datacenter, localDc);
-            assert.strictEqual(hosts[1].datacenter, localDc);
-            assert.strictEqual(hosts[2].datacenter, localDc);
-            assert.strictEqual(hosts[3].datacenter, localDc);
-            timesNext();
+          // the local replicas should be 7 (primary) and 8 in dc2.
+          var replicas = hosts.slice(0,2).map(helper.lastOctetOf).sort();
+          assert.deepEqual(replicas, ['7', '8']);
+          timesNext();
+        });
+      }, done);
+    });
+    it('should yield 1 local replica first, then 3 remaining local nodes when RF is 1', function (done) {
+      utils.times(20, function (n, timesNext) {
+        //keyspace 2
+        policy_dc2.newQueryPlan(keyspace2, {routingKey: routingKey}, function (err, iterator) {
+          var hosts = helper.iteratorToArray(iterator);
+          // 1 local replica, 3 remaining local nodes.
+          assert.ok(hosts.length, 4);
+          hosts.forEach(function(host) {
+            assert.strictEqual(host.datacenter, localDc);
           });
-        }, next);
-      },
-      function testCase3(next) {
-        utils.times(20, function (n, timesNext) {
-          //no keyspace
-          policy.newQueryPlan(null, {routingKey: new Buffer([0, 0, 0, 1])}, function (err, iterator) {
-            var hosts = helper.iteratorToArray(iterator);
-            //1 (closest) replica plus 3 additional local nodes
-            assert.ok(hosts.length, 4);
-            assert.strictEqual(hosts[0].datacenter, localDc);
-            assert.strictEqual(hosts[1].datacenter, localDc);
-            assert.strictEqual(hosts[2].datacenter, localDc);
-            assert.strictEqual(hosts[3].datacenter, localDc);
-            timesNext();
+          // the local replicas should be 3 (primary).
+          var replicas = hosts.slice(0,1).map(helper.lastOctetOf).sort();
+          assert.deepEqual(replicas, ['7']);
+          timesNext();
+        });
+      }, done);
+    });
+    it('should yield closest replica first (when same DC), then 3 remaining local nodes when RF is 1', function (done) {
+      utils.times(20, function (n, timesNext) {
+        //no keyspace
+        policy_dc1.newQueryPlan(null, {routingKey: routingKey}, function (err, iterator) {
+          var hosts = helper.iteratorToArray(iterator);
+          //1 (closest) replica irrespective of keyspace topology, plus 3 additional local nodes
+          assert.ok(hosts.length, 4);
+          hosts.forEach(function(host) {
+            assert.strictEqual(host.datacenter, 'dc1');
           });
-        }, next);
-      },
-      helper.ccmHelper.remove
-    ], done);
+          // the local replicas should be 3 (primary).
+          var replicas = hosts.slice(0,1).map(helper.lastOctetOf).sort();
+          assert.deepEqual(replicas, ['3']);
+          timesNext();
+        });
+      }, done);
+    });
+    it('should not yield closest replica when not in same DC as local', function (done) {
+      utils.times(20, function (n, timesNext) {
+        //no keyspace
+        policy_dc2.newQueryPlan(null, {routingKey: routingKey}, function (err, iterator) {
+          var hosts = helper.iteratorToArray(iterator);
+          // Should simply get all local nodes, since no keyspace was provided and the closest
+          // replica is in dc1, no token aware ordering is provided.
+          assert.ok(hosts.length, 4);
+          hosts.forEach(function(host) {
+            assert.strictEqual(host.datacenter, localDc);
+          });
+          timesNext();
+        });
+      }, done);
+    });
   });
 });

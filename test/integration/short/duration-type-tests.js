42c59e83128eacd0b1ebea38081085d60860915c
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2017-01-13 12:20:33 +0100

    Duration type support

---
+++ test/integration/short/duration-type-tests.js
@@ -0,0 +1,93 @@
+'use strict';
+var assert = require('assert');
+var helper = require('../../test-helper');
+var types = require('../../../lib/types');
+var utils = require('../../../lib/utils');
+
+var vdescribe = helper.vdescribe;
+var Duration = types.Duration;
+
+vdescribe('3.10', 'Duration', function () {
+  this.timeout('30000');
+  var setupInfo = helper.setup({
+    queries: ['CREATE TABLE tbl_duration (pk uuid PRIMARY KEY, c1 duration)']
+  });
+  var client = setupInfo.client;
+  describe('serialization', function () {
+    it('should serialize and deserialize duration type instances', function (done) {
+      var values = [
+        '1y2mo',
+        '-1y2mo',
+        '1Y2MO',
+        '2w',
+        '2d10h',
+        '2d',
+        '30h',
+        '30h20m',
+        '20m',
+        '56s',
+        '567ms',
+        '1950us',
+        '1950µs',
+        '1950000ns',
+        '1950000NS',
+        '-1950000ns',
+        '1y3mo2h10m',
+        'P1Y2D',
+        'P1Y2M',
+        'P2W',
+        'P1YT2H',
+        '-P1Y2M',
+        'P2D',
+        'PT30H',
+        'PT30H20M',
+        'PT20M',
+        'PT56S',
+        'P1Y3MT2H10M',
+        'P0001-00-02T00:00:00',
+        'P0001-02-00T00:00:00',
+        'P0001-00-00T02:00:00',
+        '-P0001-02-00T00:00:00',
+        'P0000-00-02T00:00:00',
+        'P0000-00-00T30:00:00',
+        'P0000-00-00T30:20:00',
+        'P0000-00-00T00:20:00',
+        'P0000-00-00T00:00:56',
+        'P0001-03-00T02:10:00'
+      ];
+      utils.eachSeries([ true, false ], function (prepare, prepareNext) {
+        utils.eachSeries(values, function (v, next) {
+          var query = 'INSERT INTO tbl_duration (pk, c1) VALUES (?, ?)';
+          var id = types.Uuid.random();
+          var value = Duration.fromString(v);
+          client.execute(query, [ id, value ], { prepare: prepare }, function (err) {
+            assert.ifError(err);
+            var query = 'SELECT pk, c1 FROM tbl_duration WHERE pk = ?';
+            client.execute(query, [ id ], { prepare: prepare }, function (err, result) {
+              assert.ifError(err);
+              assert.strictEqual(result.rowLength, 1);
+              var actual = result.first()['c1'];
+              helper.assertInstanceOf(actual, Duration);
+              assert.ok(actual.equals(value));
+              next();
+            });
+          });
+        }, prepareNext);
+      }, done);
+    });
+  });
+  describe('metadata', function () {
+    it('should parse column metadata', function (done) {
+      client.metadata.getTable(setupInfo.keyspace, 'tbl_duration', function (err, tableInfo) {
+        assert.ifError(err);
+        assert.ok(tableInfo);
+        assert.strictEqual(tableInfo.columns.length, 2);
+        var c1 = tableInfo.columnsByName['c1'];
+        assert.ok(c1);
+        assert.strictEqual(c1.type.code, types.dataTypes.custom);
+        assert.strictEqual(c1.type.info, 'org.apache.cassandra.db.marshal.DurationType');
+        done();
+      });
+    });
+  });
+});
---
+++ test/unit/duration-type-tests.js
@@ -0,0 +1,80 @@
+'use strict';
+var assert = require('assert');
+var util = require('util');
+var types = require('../../lib/types');
+var utils = require('../../lib/utils');
+var Duration = types.Duration;
+var Long = types.Long;
+
+var values = [
+  // [ Duration representation, hex value, standard format, ISO 8601, ISO 8601 Week
+  [ new Duration(0, 0, Long.fromNumber(1)), '000002', '1ns'],
+  [ new Duration(0, 0, Long.fromNumber(128)), '00008100', '128ns'],
+  [ new Duration(0, 0, Long.fromNumber(2)), '000004', '2ns'],
+  [ new Duration(0, 0, Long.fromNumber(256)), '00008200', '256ns'],
+  [ new Duration(0, 0, Long.fromNumber(33001)), '0000c101d2', '33us1ns'],
+  [ new Duration(0, 0, Long.fromNumber(-1)), '000001', '-1ns'],
+  [ new Duration(0, 0, Long.fromNumber(-33001)), '0000c101d1', '-33µs1ns'],
+  [ new Duration(0, 0, Long.fromNumber(0)), '000000'],
+  [ new Duration(0, 0, Long.fromNumber(2251799813685279)), '0000fe1000000000003e' ],
+  [ new Duration(-1, -1, Long.fromNumber(-1)), '010101', '-1mo1d1ns'],
+  [ new Duration(2, 15, Long.ZERO), '041e00', '2mo15d', 'P2M15D'],
+  [ new Duration(0, 14, Long.ZERO), '001c00', '14d', 'P14D', 'P2W'],
+  [ new Duration(1, 1, Long.fromNumber(1)), '020202', '1mo1d1ns'],
+  [ new Duration(257, 0, Long.fromNumber(0)), '82020000', '21y5mo', 'P21Y5M'],
+  [ new Duration(0, 2, Long.fromNumber(120000000000)), '0004f837e11d6000', '2d2m', 'P2DT2M']
+];
+
+describe('Duration', function () {
+  describe('#toBuffer()', function () {
+    it('should represent the values in vint encoding', function () {
+      values.forEach(function (item) {
+        assert.strictEqual(item[0].toBuffer().toString('hex'), item[1]);
+      });
+    });
+  });
+  describe('fromBuffer()', function () {
+    it('should decode the vint values', function () {
+      values.forEach(function (item) {
+        assertDurationEqual(Duration.fromBuffer(utils.allocBufferFromString(item[1], 'hex')), item[0], item[1]);
+      });
+    });
+  });
+  describe('fromString()', function () {
+    [
+      ['should parse standard format', 2],
+      ['should parse ISO 8601 format', 3],
+      ['should parse ISO 8601 week format', 4]
+    ].forEach(function (testInfo) {
+      it(testInfo[0], function () {
+        var index = testInfo[1];
+        values.forEach(function (item) {
+          if (!item[index]) {
+            return;
+          }
+          assertDurationEqual(Duration.fromString(item[index]), item[0], item[index]);
+        });
+      });
+    });
+  });
+  describe('#toString()', function () {
+    it('should return the standard representation', function () {
+      values.forEach(function (item) {
+        if (!item[2] || item[2].indexOf('µs') >= 0) {
+          // not standard
+          return;
+        }
+        assert.strictEqual(item[0].toString(), item[2]);
+      });
+    });
+  });
+});
+
+/**
+ * @param {Duration} actual
+ * @param {Duration} expected
+ * @param {String} input
+ */
+function assertDurationEqual(actual, expected, input) {
+  assert.ok(actual.equals(expected), util.format('%j not equals to %j, from %s', actual, expected, input));
+}

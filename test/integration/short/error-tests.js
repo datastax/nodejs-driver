f5377c9a4ca18081c2428fc56576c513020820eb
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2017-05-30 10:28:14 +0200

    Various test fixes

--- test/integration/short/error-tests.js
+++ test/integration/short/error-tests.js
@@ -12,20 +12,21 @@ var vdescribe = helper.vdescribe;
 describe('Client', function () {
   this.timeout(120000);
   vdescribe('2.2', 'with protocol v4 errors', function () {
-    var commonKs = helper.getRandomName('ks');
     var failWritesKs = helper.getRandomName('ks');
+    var failWritesTable = failWritesKs + '.tbl1';
     var setupInfo = helper.setup(2, {
-      keyspace: commonKs,
       clientOptions: {
         policies: { retry: new helper.FallthroughRetryPolicy() }
       },
       queries: [
         'CREATE TABLE read_fail_tbl(pk int, cc int, v int, primary key (pk, cc))',
+        helper.createKeyspaceCql(failWritesKs, 2, true),
+        helper.createTableCql(failWritesTable),
         helper.createKeyspaceCql('ks_func'),
         'CREATE TABLE ks_func.tbl1 (id int PRIMARY KEY, v1 int, v2 int)',
         'INSERT INTO ks_func.tbl1 (id, v1, v2) VALUES (1, 1, 0)',
-        "CREATE FUNCTION ks_func.div(a int, b int) RETURNS NULL ON NULL INPUT" +
-        " RETURNS int LANGUAGE java AS 'return a / b;'"
+        'CREATE FUNCTION ks_func.div(a int, b int) RETURNS NULL ON NULL INPUT RETURNS int LANGUAGE java AS' +
+          ' \'return a / b;\''
       ],
       ccmOptions: {
         yaml: ['tombstone_failure_threshold:1000', 'enable_user_defined_functions:true'],
@@ -37,12 +38,8 @@ describe('Client', function () {
       utils.series([
         function generateTombstones(next) {
           utils.timesSeries(2000, function (n, timesNext) {
-            client.execute('INSERT INTO read_fail_tbl (pk, cc, v) VALUES (1, ?, null)', [n], {prepare: true}, function (err, result) {
-              if (err) {
-                return next(err);
-              }
-              timesNext();
-            });
+            var query = 'INSERT INTO read_fail_tbl (pk, cc, v) VALUES (?, ?, ?)';
+            client.execute(query, [ 1, n, null ], { prepare: true }, timesNext);
           }, next);
         },
         function (next) {
@@ -65,28 +62,22 @@ describe('Client', function () {
     });
     it('should callback with writeFailure error when encountered', function (done) {
       var client = setupInfo.client;
-      var table = failWritesKs + '.tbl1';
-      utils.series([
-        helper.toTask(client.execute, client, helper.createKeyspaceCql(failWritesKs, 2, true)),
-        helper.toTask(client.execute, client, helper.createTableCql(table)),
-        function (next) {
-          var query = util.format('INSERT INTO %s (id, text_sample) VALUES (?, ?)', table);
-          client.execute(query, [types.Uuid.random(), '1'], { consistency: types.consistencies.all}, function (err) {
-            helper.assertInstanceOf(err, errors.ResponseError);
-            assert.strictEqual(err.code, types.responseErrorCodes.writeFailure);
-            if (client.controlConnection.protocolVersion >= protocolVersion.v5) {
-              assert.strictEqual(typeof err.reasons, 'object');
-              Object.keys(err.reasons).forEach(function (key) {
-                assert.strictEqual(typeof err.reasons[key], 'number');
-              });
-            }
-            else {
-              assert.strictEqual(err.failures, 1);
-            }
-            assert.strictEqual(err.writeType, 'SIMPLE');
-            next();
+      var query = util.format('INSERT INTO %s (id, text_sample) VALUES (?, ?)', failWritesTable);
+      client.execute(query, [ types.Uuid.random(), '1' ], { consistency: types.consistencies.all }, function (err) {
+        helper.assertInstanceOf(err, errors.ResponseError);
+        assert.strictEqual(err.code, types.responseErrorCodes.writeFailure);
+        if (client.controlConnection.protocolVersion >= protocolVersion.v5) {
+          assert.strictEqual(typeof err.reasons, 'object');
+          Object.keys(err.reasons).forEach(function (key) {
+            assert.strictEqual(typeof err.reasons[key], 'number');
           });
-        }], done);
+        }
+        else {
+          assert.strictEqual(err.failures, 1);
+        }
+        assert.strictEqual(err.writeType, 'SIMPLE');
+        done();
+      });
     });
     it('should callback with functionFailure error when the cql function throws an error', function (done) {
       var client = setupInfo.client;

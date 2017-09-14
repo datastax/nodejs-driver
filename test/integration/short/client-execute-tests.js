178b7fe1526b3b98181aa2d1fe13b8a3cb25adec
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2017-09-14 11:53:45 +0200

    Add wasApplied() method to ResultSet

--- test/integration/short/client-execute-tests.js
+++ test/integration/short/client-execute-tests.js
@@ -9,6 +9,7 @@ var types = require('../../../lib/types');
 var utils = require('../../../lib/utils.js');
 var errors = require('../../../lib/errors.js');
 var vit = helper.vit;
+var vdescribe = helper.vdescribe;
 
 describe('Client', function () {
   this.timeout(120000);
@@ -960,6 +961,31 @@ describe('Client', function () {
           .catch(function (err) {
             helper.assertInstanceOf(err, errors.ResponseError);
           });
+      });
+    });
+    vdescribe('2.0', 'with lightweight transactions', function () {
+      var client = setupInfo.client;
+      var id = types.Uuid.random();
+      before(function (done) {
+        var query = util.format('INSERT INTO %s (id, text_sample) VALUES (?, ?)', table);
+        client.execute(query, [id, 'val' ], done);
+      });
+      [
+        [ 'is not a conditional update', true, 'INSERT INTO %s (id, text_sample) VALUES (?, ?)', [ id, 'val'] ],
+        [ 'is a conditional update and it was applied', true,
+          'INSERT INTO %s (id, text_sample) VALUES (?, ?) IF NOT EXISTS', [ types.Uuid.random(), 'val2'] ],
+        [ 'is a conditional update and it was not applied', false,
+          'INSERT INTO %s (id, text_sample) VALUES (?, ?) IF NOT EXISTS', [ id, 'val'] ]
+      ].forEach(function (item) {
+        context('when it ' + item[0], function () {
+          it('should return a ResultSet with wasApplied set to ' + item[1], function (done) {
+            client.execute(util.format(item[2], table), item[3], function (err, result) {
+              assert.ifError(err);
+              assert.strictEqual(result.wasApplied(), item[1]);
+              done();
+            });
+          });
+        });
       });
     });
   });

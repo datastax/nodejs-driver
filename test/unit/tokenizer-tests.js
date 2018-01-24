ab065a6e193b00e7f57427a73a1cc4245790a6c5
Andrew Tolbert <andrew.tolbert@datastax.com>
2018-01-24 14:02:57 -0600

    Expose node token and range information (#261)
    Also addresses NODEJS-419: Fix tokenToReplica map for vnodes

--- test/unit/tokenizer-tests.js
+++ test/unit/tokenizer-tests.js
@@ -2,8 +2,10 @@
 const assert = require('assert');
 
 const tokenizer = require('../../lib/tokenizer');
+const token = require('../../lib/token');
 const Murmur3Tokenizer = tokenizer.Murmur3Tokenizer;
 const RandomTokenizer = tokenizer.RandomTokenizer;
+const ByteOrderedTokenizer = tokenizer.ByteOrderedTokenizer;
 const types = require('../../lib/types');
 const utils = require('../../lib/utils');
 const MutableLong = require('../../lib/types/mutable-long');
@@ -72,30 +74,51 @@ describe('Murmur3Tokenizer', function () {
         [[226, 231, 226, 231, 226, 231, 1], '2222373981930033306']
       ].forEach(function (item) {
         const v = t.hash(item[0]);
-        assert.ok(v.equals(MutableLong.fromString(item[1])));
+        helper.assertInstanceOf(v, token.Murmur3Token);
+        assert.strictEqual(v.getValue().toString(), item[1]);
       });
     });
   });
-  describe('RandomTokenizer', function () {
-    const t = new RandomTokenizer();
-    describe('#hash', function () {
-      it('should return expected results', function () {
-        [
-          [[1, 2, 3, 4], '11748876857495436398853550283091289647'],
-          [[1, 2, 3, 4, 5, 6], '141904934057871337334287797400233978956'],
-          [utils.allocBufferFromString('fffafa000102030405fe', 'hex'), '93979376327542758013347018124903879310'],
-          [utils.allocBufferFromString('f000ee0000', 'hex'), '155172302213453714586395175393246848871']
-        ].forEach(function (item) {
-          assert.strictEqual(t.hash(item[0]).toString(), item[1]);
-        });
+});
+
+describe('RandomTokenizer', function () {
+  const t = new RandomTokenizer();
+  describe('#hash', function () {
+    it('should return expected results', function () {
+      [
+        [[1, 2, 3, 4], '11748876857495436398853550283091289647'],
+        [[1, 2, 3, 4, 5, 6], '141904934057871337334287797400233978956'],
+        [utils.allocBufferFromString('fffafa000102030405fe', 'hex'), '93979376327542758013347018124903879310'],
+        [utils.allocBufferFromString('f000ee0000', 'hex'), '155172302213453714586395175393246848871']
+      ].forEach(function (item) {
+        assert.strictEqual(t.hash(item[0]).toString(), item[1]);
       });
     });
-    describe('#parse()', function () {
-      it('should return the Integer representation', function () {
-        const val = t.parse('141904934057871337334287797400233978956');
-        helper.assertInstanceOf(val, types.Integer);
-        assert.ok(val.equals(types.Integer.fromString('141904934057871337334287797400233978956')));
-      });
+  });
+  describe('#parse()', function () {
+    it('should return the Integer representation', function () {
+      const val = t.parse('141904934057871337334287797400233978956');
+      helper.assertInstanceOf(val, token.RandomToken);
+      helper.assertInstanceOf(val.getValue(), types.Integer);
+      assert.ok(val.getValue().equals(types.Integer.fromString('141904934057871337334287797400233978956')));
+    });
+  });
+});
+
+describe('ByteOrderedTokenizer', function() {
+  const t = new ByteOrderedTokenizer();
+  describe('#parse()', function() {
+    it('should strip trailing 0-bytes', function () {
+      const val = t.parse('040000');
+      helper.assertInstanceOf(val, token.ByteOrderedToken);
+      helper.assertInstanceOf(val.getValue(), Buffer);
+      assert.strictEqual(val.toString(), '04');
+    });
+    it('should not strip trailing bytes that are not 0', function () {
+      const val = t.parse('04000007');
+      helper.assertInstanceOf(val, token.ByteOrderedToken);
+      helper.assertInstanceOf(val.getValue(), Buffer);
+      assert.strictEqual(val.toString(), '04000007');
     });
   });
 });

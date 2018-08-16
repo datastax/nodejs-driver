19660c0384ca58eccb444fc0084030279265a205
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-08-16 17:29:06 +0200

    Insert and order by

--- test/integration/short/mapper/mapper-tests.js
+++ test/integration/short/mapper/mapper-tests.js
@@ -5,8 +5,13 @@ const helper = require('../../../test-helper');
 const Client = require('../../../../lib/client');
 const utils = require('../../../../lib/utils');
 const types = require('../../../../lib/types');
+const Uuid = types.Uuid;
 const Mapper = require('../../../../lib/mapper/mapper');
 const TableMappingInfo = require('../../../../lib/mapper/table-mapping-info');
+
+const colsToProps = new Map([
+  ['videoid', 'id'], ['userid', 'userId'], ['added_date', 'addedDate'], ['location_type', 'locationType'],
+  ['preview_image_location', 'preview'], ['preview_thumbnails', 'thumbnails']]);
 
 describe('Mapper', function () {
   this.timeout(60000);
@@ -43,13 +48,13 @@ describe('Mapper', function () {
   ];
 
   const setupInfo = helper.setup(1, { queries });
-  //const setupInfo = { keyspace: 'ks0507795538724718' };
 
   context('with videos tables', () => {
-    // videos -> video id
+    // videos -> videoid
     // user_videos -> userid, added_date, videoid
     // latest_videos -> yyyymmdd, added_date, videoid
     const mapper = getMapper(setupInfo);
+    const client = mapper.client;
     const videoMapper = mapper.forModel('Video', getVideosMappingInfo(setupInfo));
 
     describe('#find()', () => {
@@ -66,10 +71,86 @@ describe('Mapper', function () {
           assert.ok(result.first());
         });
       });
+
+      it('should use the provided fields and order by when defined', () => {
+        const doc = { yyyymmdd: 'NOTEXISTENT' };
+        return videoMapper.find(doc, { fields: ['id', 'yyyymmdd', 'name'], orderBy: { 'addedDate': 'asc'} }, 'default')
+          .then(result => {
+            assert.strictEqual(result.first(), null);
+          });
+      });
+    });
+
+    describe('#insert()', () => {
+      it('should insert on all tables where the partition and clustering keys are specified', () => {
+        const doc = {
+          id: Uuid.random(), userId: Uuid.random(), addedDate: new Date(), name: 'Video insert sample 1',
+          description: 'description insert 1', yyyymmdd: new Date().toISOString().substr(0, 10), tags: ['a', 'b'],
+          location: 'a/b/c', locationType: 1, preview: 'a/preview/c', thumbnails: { 'p1': 'd/e/f' }
+        };
+
+        return videoMapper.insert(doc, null, 'default')
+          .then(() => getVideoRows(client, doc))
+          .then(rows => {
+            // It should have been inserted on the 3 tables
+            assert.strictEqual(rows.length, 3);
+            rows.forEach(row => assertRowMatchesDoc(row, doc));
+          });
+      });
+
+      it('should insert on some of the tables when those keys are not specified', () => {
+        const doc = {
+          id: Uuid.random(), userId: Uuid.random(), addedDate: new Date(), name: 'Video insert 2 not in latest',
+          description: 'This video will not be added to latest_videos table',
+        };
+
+        return videoMapper.insert(doc, null, 'default')
+          .then(() => getVideoRows(client, doc))
+          .then(rows => {
+            // Inserted on "videos" and "user_videos" tables
+            assert.strictEqual(rows.length, 2);
+            rows.forEach(row => assertRowMatchesDoc(row, doc));
+          });
+      });
+
+      it('should insert a single table when it only matches one table', () => {
+        const doc = { id: Uuid.random(), name: 'Video insert 3' };
+
+        return videoMapper.insert(doc, null, 'default')
+          .then(() => getVideoRows(client, doc))
+          .then(rows => {
+            // Inserted only on "videos" table
+            assert.strictEqual(rows.length, 1);
+            rows.forEach(row => assertRowMatchesDoc(row, doc));
+          });
+      });
+
+      it('should consider fields filter', () => {
+        const doc = {
+          id: Uuid.random(), userId: Uuid.random(), addedDate: new Date(), name: 'Video insert sample 4',
+          description: 'description insert 4', yyyymmdd: new Date().toISOString().substr(0, 10), locationType: 1,
+          preview: 'a/preview/c'
+        };
+
+        return videoMapper.insert(doc, { fields: ['id', 'userId', 'addedDate', 'name']}, 'default')
+          .then(() => getVideoRows(client, doc))
+          .then(rows => {
+            // Inserted on "videos" and "user_videos" tables
+            assert.strictEqual(rows.length, 3);
+            // It retrieved a empty ResultSet
+            assert.strictEqual(rows[2], null);
+
+            // Use a doc with undefined values for fields not included
+            const expectedDoc = { id: doc.id, userId: doc.userId, addedDate: doc.addedDate, name: doc.name };
+
+            rows.slice(0, 2).forEach(row => assertRowMatchesDoc(row, expectedDoc));
+          });
+      });
     });
   });
 });
 
+/** @return {Mapper} */
 function getMapper(setupInfo) {
   const client = new Client(utils.extend({ keyspace: setupInfo.keyspace }, helper.baseOptions));
   before(() => client.connect());
@@ -84,6 +165,37 @@ function getVideosMappingInfo(setupInfo) {
     { name: 'latest_videos', isView: false }
   ];
 
-  const columns = new Map([['videoid', 'id'], ['userid', 'userId']]);
-  return new TableMappingInfo(setupInfo.keyspace, tables, null, columns);
+  return new TableMappingInfo(setupInfo.keyspace, tables, null, colsToProps);
+}
+
+function getPropertyName(columnName) {
+  const mappedName = colsToProps.get(columnName);
+  return mappedName === undefined ? columnName : mappedName;
+}
+
+function assertRowMatchesDoc(row, doc) {
+  assert.ok(row);
+  Object.keys(row).forEach(columnName => {
+    assert.deepEqual(row[columnName], doc[getPropertyName(columnName)]);
+  });
+}
+
+/** @returns {Promise<Array<Row>>} */
+function getVideoRows(client, doc) {
+  const queries = [
+    ['SELECT * FROM videos WHERE videoid = ?', [doc.id]]
+  ];
+
+  if (doc.userId && doc.addedDate) {
+    queries.push(['SELECT * FROM user_videos WHERE userid = ? AND added_date = ? AND videoid = ?',
+      [doc.userId, doc.addedDate, doc.id]]);
+  }
+
+  if (doc.yyyymmdd && doc.addedDate) {
+    queries.push(['SELECT * FROM latest_videos WHERE yyyymmdd = ? AND added_date = ? AND videoid = ?',
+      [doc.yyyymmdd, doc.addedDate, doc.id]]);
+  }
+
+  return Promise.all(queries.map(q => client.execute(q[0], q[1], {prepare: true})))
+    .then(results => results.map(rs => rs.first()));
 }

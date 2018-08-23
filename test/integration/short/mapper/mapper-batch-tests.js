141fee72c6fb513b707f9e8e98ae60728fe845fa
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-08-23 17:08:26 +0200

    Batch fixes and tests

--- test/integration/short/mapper/mapper-batch-tests.js
+++ test/integration/short/mapper/mapper-batch-tests.js
@@ -16,6 +16,7 @@ describe('Mapper', function () {
     const mapper = mapperTestHelper.getMapper();
     const client = mapper.client;
     const videoMapper = mapper.forModel('Video', mapperTestHelper.getVideosMappingInfo());
+    const userMapper = mapperTestHelper.getUserModelMapper(mapper);
 
     it('should execute a batch containing updates to multiple tables from the same doc', () => {
       const doc = { id: Uuid.random(), userId: Uuid.random(), addedDate: new Date(), name: 'hello!' };
@@ -37,15 +38,13 @@ describe('Mapper', function () {
 
       const columns = 'videoid, userid, added_date, name';
 
-      const items = [
-        videoMapper.batching.update(docs[0]),
-        videoMapper.batching.insert(docs[1]),
-        videoMapper.batching.remove(docs[2], { fields: [ 'id', 'userId', 'addedDate' ]})
-      ];
-
       // Insert the rows that are going to be used for DELETE
       return mapperTestHelper.insertVideoRows(client, docs[2])
-        .then(() => mapper.batch(items))
+        .then(() => mapper.batch([
+          videoMapper.batching.update(docs[0]),
+          videoMapper.batching.insert(docs[1]),
+          videoMapper.batching.remove(docs[2], { fields: [ 'id', 'userId', 'addedDate' ]})
+        ]))
         .then(() => Promise.all([
           mapperTestHelper.getVideoRows(client, docs[0], columns),
           mapperTestHelper.getVideoRows(client, docs[1], columns),
@@ -63,6 +62,30 @@ describe('Mapper', function () {
               rows.forEach(row => assert.strictEqual(row, null));
             }
           });
+        });
+    });
+
+    it('should execute a batch containing mutations from multiple docs', () => {
+      const videoDoc = { id: Uuid.random(), userId: Uuid.random(), addedDate: new Date(), name: 'new doc' };
+      const userDoc = { id: Uuid.random(), firstName: 'hey', lastName: 'joe', email: 'hey@joe.com' };
+
+      return mapper
+        .batch([
+          videoMapper.batching.update(videoDoc),
+          userMapper.batching.update(userDoc)
+        ])
+        .then(() => Promise.all([
+          mapperTestHelper.getVideoRows(client, videoDoc, 'videoid, userid, added_date, name'),
+          client.execute('SELECT * FROM users WHERE userid = ?', [ userDoc.id ], { prepare: true })
+            .then(result => result.rows)
+        ]))
+        .then(results => {
+          const videoRows = results[0];
+          assert.strictEqual(videoRows.length, 2);
+          videoRows.forEach(row => assertRowMatchesDoc(row, videoDoc));
+          const userRows = results[1];
+          assert.strictEqual(userRows.length, 1);
+          assert.strictEqual(userRows[0]['firstname'], userDoc.firstName);
         });
     });
   });
--- test/integration/short/mapper/mapper-test-helper.js
+++ test/integration/short/mapper/mapper-test-helper.js
@@ -9,9 +9,8 @@ const Mapper = require('../../../../lib/mapper/mapper');
 const Client = require('../../../../lib/client');
 const utils = require('../../../../lib/utils');
 
-const colsToProps = new Map([
-  ['videoid', 'id'], ['userid', 'userId'], ['added_date', 'addedDate'], ['location_type', 'locationType'],
-  ['preview_image_location', 'preview'], ['preview_thumbnails', 'thumbnails']]);
+const colsToProps = new Map([ ['videoid', 'id'], ['userid', 'userId'], ['added_date', 'addedDate'],
+  ['location_type', 'locationType'], ['preview_image_location', 'preview'], ['preview_thumbnails', 'thumbnails']]);
 
 let hasBeenSetup = false;
 
@@ -27,13 +26,18 @@ const mapperHelper = module.exports = {
     hasBeenSetup = true;
 
     const queries = [
-      `CREATE TABLE videos (videoid uuid, userid uuid, name varchar, description varchar, location text,location_type int,
-     preview_thumbnails map<text,text>, tags set<varchar>, added_date timestamp,PRIMARY KEY (videoid))`,
-      `CREATE TABLE user_videos (userid uuid, added_date timestamp, videoid uuid, name text, preview_image_location text,
-     PRIMARY KEY (userid, added_date, videoid)) WITH CLUSTERING ORDER BY (added_date DESC, videoid ASC)`,
+      // Create tables
+      `CREATE TABLE videos (videoid uuid, userid uuid, name varchar, description varchar, location text,
+      location_type int, preview_thumbnails map<text,text>, tags set<varchar>, added_date timestamp,
+      PRIMARY KEY (videoid))`,
+      `CREATE TABLE user_videos (userid uuid, added_date timestamp, videoid uuid, name text,
+      preview_image_location text, PRIMARY KEY (userid, added_date, videoid))
+      WITH CLUSTERING ORDER BY (added_date DESC, videoid ASC)`,
       `CREATE TABLE latest_videos (yyyymmdd text, added_date timestamp, videoid uuid, name text,
-     preview_image_location text, PRIMARY KEY (yyyymmdd, added_date, videoid))
-     WITH CLUSTERING ORDER BY (added_date DESC, videoid ASC)`,
+      preview_image_location text, PRIMARY KEY (yyyymmdd, added_date, videoid))
+      WITH CLUSTERING ORDER BY (added_date DESC, videoid ASC)`,
+      `CREATE TABLE users (userid uuid, firstname varchar, lastname varchar, email text, created_date timestamp,
+      PRIMARY KEY (userid))`,
 
       // Insert test data
       `INSERT INTO videos (videoid, name, userid, description, location, location_type, preview_thumbnails, tags,
@@ -122,6 +126,17 @@ const mapperHelper = module.exports = {
     ];
 
     return new TableMappingInfo(mapperHelper.keyspace, tables, null, colsToProps);
+  },
+
+  getUserModelMapper(mapper) {
+    const tables = [
+      { name: 'users', isView: false }
+    ];
+
+    const cols = new Map([['videoid', 'id'], ['userid', 'id'], ['created_date', 'createdDate'],
+      ['firstname', 'firstName'], ['lastname', 'lastName']]);
+
+    return mapper.forModel('User', new TableMappingInfo(mapperHelper.keyspace, tables, null, cols));
   },
 
   getPropertyName: function (columnName) {

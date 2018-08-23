1e9dfee4456b2cb379818fb263cec13068927fb4
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-08-23 12:11:18 +0200

    Basic batch update support

--- test/integration/short/mapper/mapper-batch-tests.js
+++ test/integration/short/mapper/mapper-batch-tests.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const assert = require('assert');
+const types = require('../../../../lib/types');
+const Uuid = types.Uuid;
 const mapperTestHelper = require('./mapper-test-helper');
+const assertRowMatchesDoc = mapperTestHelper.assertRowMatchesDoc;
 
 
 describe('Mapper', function () {
@@ -9,8 +13,19 @@ describe('Mapper', function () {
 
   describe('#batch()', () => {
 
-    it('should execute a batch containing multiple tables from the same doc', () => {
+    const mapper = mapperTestHelper.getMapper();
+    const client = mapper.client;
+    const videoMapper = mapper.forModel('Video', mapperTestHelper.getVideosMappingInfo());
 
+    it('should execute a batch containing updates to multiple tables from the same doc', () => {
+      const doc = { id: Uuid.random(), userId: Uuid.random(), addedDate: new Date(), name: 'hello!' };
+
+      return mapper.batch([ videoMapper.batching.update(doc) ])
+        .then(() => mapperTestHelper.getVideoRows(client, doc, 'videoid, userid, added_date, name'))
+        .then(rows => {
+          assert.strictEqual(rows.length, 2);
+          rows.forEach(row => assertRowMatchesDoc(row, doc));
+        });
     });
   });
 });

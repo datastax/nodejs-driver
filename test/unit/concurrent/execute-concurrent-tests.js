16fca47eb1ab81406f868b64606895fc1bf26a55
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2019-04-09 11:55:36 +0200

    StreamBasedExecutor implementation and unit tests

--- test/unit/concurrent/execute-concurrent-tests.js
+++ test/unit/concurrent/execute-concurrent-tests.js
@@ -1,14 +1,30 @@
 'use strict';
 
 const assert = require('assert');
+const Readable = require('stream').Readable;
 const types = require('../../../lib/types');
+const utils = require('../../../lib/utils');
 const helper = require('../../test-helper');
 const executeConcurrent = require('../../../lib/concurrent').executeConcurrent;
 
 describe('executeConcurrent(client, query, parameters)', function () {
   this.timeout(10000);
 
-  it('should validate parameters');
+  it('should validate parameters', () => {
+    assert.throws(() => executeConcurrent(null), TypeError, /Client instance is not defined/);
+
+    assert.throws(() => executeConcurrent({}, null), TypeError,
+      /A string query or query and parameters array should be provided/);
+
+    assert.throws(() => executeConcurrent({}, {}), TypeError,
+      /A string query or query and parameters array should be provided/);
+
+    assert.throws(() => executeConcurrent({}, 'SELECT ...'), TypeError,
+      /parameters should be an Array or a Stream instance/);
+
+    assert.throws(() => executeConcurrent({}, 'SELECT ...', {}), TypeError,
+      /parameters should be an Array or a Stream instance/);
+  });
 
   it('should support a higher concurrency level than the number of items', () => {
     const testContext = getTestContext();
@@ -53,7 +69,14 @@ describe('executeConcurrent(client, query, parameters)', function () {
       });
   });
 
-  context('with collectResults set to true', () => {
+  it('should support empty parameters array', () => {
+    const testContext = getTestContext();
+
+    return executeConcurrent(testContext.client, 'Q1', [])
+      .then(result => assert.strictEqual(result.totalExecuted, 0));
+  });
+
+  context('when collectResults is true', () => {
 
     it('should set the resultItems', () => {
       const testContext = getTestContext();
@@ -77,7 +100,7 @@ describe('executeConcurrent(client, query, parameters)', function () {
     });
   });
 
-  context('with collectResults set to false', () => {
+  context('when collectResults is false', () => {
     it('should throw when accessing resultItems property', () => {
       const testContext = getTestContext();
       const parameters = Array.from(Array(10).keys()).map(x => [ x, 0 ]);
@@ -88,7 +111,7 @@ describe('executeConcurrent(client, query, parameters)', function () {
     });
   });
 
-  context('with raiseOnFirstError set to true', () => {
+  context('when raiseOnFirstError is true', () => {
     it('should throw when first error is encountered', () => {
       const testContext = getTestContext([ 53, 80 ]);
       const concurrencyLevel = 10;
@@ -108,7 +131,7 @@ describe('executeConcurrent(client, query, parameters)', function () {
     });
   });
 
-  context('with raiseOnFirstError set to false', () => {
+  context('when raiseOnFirstError is false', () => {
     it('should continue executing when first error is encountered', () => {
       const errorIndexes = [ 23, 81 ];
       const testContext = getTestContext(errorIndexes);
@@ -180,6 +203,216 @@ describe('executeConcurrent(client, query, parameters)', function () {
   });
 });
 
+describe('executeConcurrent(client, query, stream)', () => {
+  it('should support an empty stream', () => {
+    const testContext = getTestContext();
+    const stream = new TestStream([]);
+
+    return executeConcurrent(testContext.client, 'INSERT...', stream)
+      .then(result => {
+        assert.strictEqual(result.totalExecuted, 0);
+        assert.strictEqual(testContext.index, 0);
+      });
+  });
+
+  it('should throw when the stream emits an error', () => {
+    const testContext = getTestContext();
+    const testError = new Error('Test error');
+    const stream = new TestStream([[[1, 2], [3, 4]], [[5, 6], testError, [9, 10]]]);
+    let error;
+
+    return executeConcurrent(testContext.client, 'INSERT...', stream)
+      .catch(err => error = err)
+      .then(() => {
+        assert.strictEqual(error, testError);
+      });
+  });
+
+  [
+    { params: [['a']], description: 'when there is an invalid item on the first position' },
+    { params: [[[1, 2], 'a']], description: 'when there is an invalid item on the second position' },
+    { params: [[[1, 2], [2, 3]], ['a']], description: 'when there is an invalid item pushed later in time' }
+  ].forEach(item => {
+    it(`should throw when items are not Array instances ${item.description}`, () => {
+      const testContext = getTestContext();
+      const stream = new TestStream(item.params);
+      let error;
+
+      return executeConcurrent(testContext.client, 'INSERT...', stream)
+        .catch(err => error = err)
+        .then(() => {
+          helper.assertInstanceOf(error, TypeError);
+          assert.strictEqual(error.message, 'Stream should be in objectMode and should emit Array instances');
+        });
+    });
+  });
+
+  it('should execute one query per item in the stream', () => {
+    const testContext = getTestContext();
+    const stream = new TestStream([[[1, 2], [3, 4]], [[5, 6], [7, 8], [9, 10]]], { endAsync: true });
+
+    return executeConcurrent(testContext.client, 'INSERT...', stream)
+      .then(result => {
+        assert.strictEqual(result.totalExecuted, 5);
+        assert.strictEqual(testContext.index, 5);
+      });
+  });
+
+  it('should support ending right after the last item is read', () => {
+    const testContext = getTestContext();
+    const stream = new TestStream([[['a', 'b'], ['c', 'd']], [['e', 'f']]], { endAsync: false });
+
+    return executeConcurrent(testContext.client, 'INSERT...', stream)
+      .then(result => {
+        assert.strictEqual(result.totalExecuted, 3);
+        assert.strictEqual(testContext.index, 3);
+      });
+  });
+
+  it('should stop reading according to the concurrency level', () => {
+    const testContext = getTestContext();
+    const values = Array.from(new Array(100).keys()).map(x => [x]);
+    const stream = new TestStream([ values ]);
+    const concurrencyLevel = 8;
+
+    return executeConcurrent(testContext.client, 'INSERT...', stream, { concurrencyLevel })
+      .then(result => {
+        assert.strictEqual(result.totalExecuted, values.length);
+        assert.strictEqual(testContext.index, values.length);
+        // We should validate that there was a high number of pauses.
+        // The exact number of pauses depends on how fast client.execute() is
+        assert.ok(stream.pauseCounter > Math.floor(values.length / concurrencyLevel) - 1);
+      });
+  });
+
+  context('when collectResults is false', () => {
+    it('should throw when accessing resultItems property', () => {
+      const testContext = getTestContext();
+      const stream = new TestStream([[['a', 'b'], ['c', 'd']], [['e', 'f']]], { endAsync: false });
+
+      return executeConcurrent(testContext.client, 'Q1', stream, { collectResults: false })
+        .then(result => assert.throws(() => result.resultItems,
+          /Property resultItems can not be accessed when collectResults is set to false/));
+    });
+  });
+
+  context('when collectResults is true', () => {
+    it('should set the resultItems', () => {
+      const testContext = getTestContext();
+      const values = Array.from(new Array(100).keys()).map(x => [x]);
+      const stream = new TestStream([ values.slice(0, 2), values.slice(2, 10), values.slice(10) ]);
+
+      return executeConcurrent(testContext.client, 'INSERT...', stream, { concurrencyLevel: 8, collectResults: true })
+        .then(result => {
+          assert.strictEqual(result.totalExecuted, values.length);
+          assert.strictEqual(result.errors.length, 0);
+          assert.ok(stream.pauseCounter > 0);
+          result.resultItems.forEach((rs, index) => {
+            helper.assertInstanceOf(rs, types.ResultSet);
+            assert.ok(Array.isArray(rs.rows));
+            assert.deepStrictEqual(rs.rows[0]['col1'], [ index ]);
+          });
+        });
+    });
+  });
+
+  context('when raiseOnFirstError is true', () => {
+    it('should throw when first error is encountered', () => {
+      const testContext = getTestContext([23, 81]);
+      const values = Array.from(new Array(100).keys()).map(x => [x]);
+      const stream = new TestStream([ values ]);
+      const concurrencyLevel = 5;
+      let error;
+
+      return executeConcurrent(testContext.client, 'Q1', stream, { raiseOnFirstError: true, concurrencyLevel })
+        .catch(err => error = err)
+        .then(() => {
+          helper.assertInstanceOf(error, Error);
+          assert.strictEqual(error.message, 'Test error 23');
+          return new Promise(r => setTimeout(r, 100));
+        })
+        // It should not continue executing
+        .then(() => assert.ok(testContext.index < 70));
+    });
+  });
+
+  context('when raiseOnFirstError is false', () => {
+    it('should continue executing when first error is encountered', () => {
+      const errorIndexes = [ 23, 81 ];
+      const testContext = getTestContext(errorIndexes);
+      const values = Array.from(Array(100).keys()).map(x => [ x, 0 ]);
+      const stream = new TestStream([ values ]);
+
+      let error;
+
+      return executeConcurrent(testContext.client, 'Q1', stream, { raiseOnFirstError: false })
+        .catch(err => error = err)
+        .then(result => {
+          assert.strictEqual(error, undefined);
+          assert.ok(result);
+          assert.strictEqual(result.totalExecuted, values.length);
+          assert.strictEqual(result.errors.length, 2);
+          assert.deepStrictEqual(result.errors.map(e => e.message), errorIndexes.map(i => `Test error ${i}`) );
+        });
+    });
+
+    it('should set the errors in the resultItems when collectResults is set to true', () => {
+      const errorIndexes = [ 13, 70 ];
+      const testContext = getTestContext(errorIndexes);
+      const values = Array.from(Array(100).keys()).map(x => [ x, 0 ]);
+      const stream = new TestStream([ values ]);
+      const options = { raiseOnFirstError: false, collectResults: true, concurrencyLevel: 8 };
+      let error;
+
+      return executeConcurrent(testContext.client, 'Q1', stream, options)
+        .catch(err => error = err)
+        .then(result => {
+          assert.strictEqual(error, undefined);
+          assert.ok(result);
+          assert.strictEqual(result.totalExecuted, values.length);
+          assert.strictEqual(result.errors.length, 2);
+          assert.deepStrictEqual(result.errors.map(e => e.message), errorIndexes.map(i => `Test error ${i}`) );
+          assert.strictEqual(result.resultItems.length, values.length);
+
+          result.resultItems.forEach((rs, index) => {
+            if (errorIndexes.indexOf(index) === -1) {
+              helper.assertInstanceOf(rs, types.ResultSet);
+              assert.ok(Array.isArray(rs.rows));
+            } else {
+              helper.assertInstanceOf(rs, Error);
+              helper.assertContains(rs.message, 'Test error');
+            }
+          });
+        });
+    });
+
+    it('should stop collecting errors when maxErrors is exceeded', () => {
+      const errorIndexes = Array.from(Array(100).keys()).slice(31, 46);
+      const testContext = getTestContext(errorIndexes);
+      const values = Array.from(Array(100).keys()).map(x => [ x, x ]);
+      const stream = new TestStream([ values.slice(0, 10), values.slice(10) ]);
+      const options = { raiseOnFirstError: false, maxErrors: 10 };
+      let error;
+
+      return executeConcurrent(testContext.client, 'Q1', stream, options)
+        .catch(err => error = err)
+        .then(result => {
+          assert.strictEqual(error, undefined);
+          assert.ok(result);
+          assert.strictEqual(result.totalExecuted, values.length);
+          assert.strictEqual(result.errors.length, options.maxErrors);
+          assert.deepStrictEqual(
+            result.errors.map(e => e.message),
+            errorIndexes.slice(0, 10).map(i => `Test error ${i}`) );
+        });
+    });
+  });
+});
+
+describe('executeConcurrent(client, queryAndParameters)', () => {
+  it('should be implemented');
+});
+
 function getTestContext(errorIndexes) {
   errorIndexes = errorIndexes || [];
 
@@ -188,7 +421,7 @@ function getTestContext(errorIndexes) {
     inFlight: 0,
     index: 0,
     client: {
-      execute: function (query, parameters, options) {
+      execute: function (query, parameters) {
         const index = testContext.index++;
         testContext.maxInFlight = Math.max(++testContext.inFlight, testContext.maxInFlight);
 
@@ -206,4 +439,49 @@ function getTestContext(errorIndexes) {
   };
 
   return testContext;
+}
+
+class TestStream extends Readable {
+
+  constructor(values, options) {
+    super({ objectMode: true });
+
+    options = options || utils.emptyObject;
+    this.pauseCounter = 0;
+    this.resumeCounter = 0;
+    const baseDelay = 20;
+    const endAsync = typeof options.endAsync === 'boolean' ? options.endAsync : true;
+
+    if (values) {
+      values.forEach((parametersGroups, i) => {
+        setTimeout(() => {
+          parametersGroups.forEach(parameters => {
+            if (parameters instanceof Error) {
+              return this.emit('error', parameters);
+            }
+
+            this.push(parameters);
+          });
+
+          if (i === values.length - 1 && !endAsync) {
+            this.push(null);
+          }
+        }, baseDelay * i);
+      });
+
+      if (endAsync) {
+        // Signal end on timeout
+        setTimeout(() => this.push(null), baseDelay * values.length);
+      }
+    }
+  }
+
+  _read() {
+
+  }
+
+  pause() {
+    this.pauseCounter++;
+    super.pause();
+  }
 }

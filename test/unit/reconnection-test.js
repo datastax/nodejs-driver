cab63fc38ba11174c7c5334163fae764261b8c0c
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2019-04-23 09:52:38 +0200

    ExponentialReconnectionPolicy: add jitter to delays

--- test/unit/reconnection-test.js
+++ test/unit/reconnection-test.js
@@ -27,21 +27,60 @@ describe('ConstantReconnectionPolicy', function () {
 });
 
 describe('ExponentialReconnectionPolicy', function () {
-  it('should exponentially grow the wait time', function (done) {
+  it('should exponentially grow the wait time', function () {
     const baseDelay = 1000;
     const maxDelay = 256000;
     const policy = new reconnection.ExponentialReconnectionPolicy(baseDelay, maxDelay, false);
-    utils.times(1, function (n, next) {
-      const schedule = policy.newSchedule();
-      for (let i = 0; i < 8; i++) {
-        assert.strictEqual(schedule.next().value, Math.pow(2, i + 1) * baseDelay);
-      }
-      for (let j = 8; j < 30; j++) {
-        assert.strictEqual(schedule.next().value, maxDelay);
-      }
-      next();
-    }, done);
 
+    const schedule = policy.newSchedule();
+
+    let value = schedule.next().value;
+    assert.ok(value >= baseDelay && value < baseDelay * 1.15, `Value is incorrect: ${value}`);
+
+    for (let i = 1; i < 8; i++) {
+      value = schedule.next().value;
+      const expectedDelay = Math.pow(2, i) * baseDelay;
+      assert.ok(value >= expectedDelay * 0.85 && value < expectedDelay * 1.15, `Value is incorrect: ${value}`);
+    }
+
+    for (let j = 8; j < 30; j++) {
+      value = schedule.next().value;
+      assert.ok(value >= maxDelay * 0.85 && value < maxDelay, `Value is incorrect: ${value}`);
+    }
+  });
+
+  it('should return independent schedules', () => {
+    const baseDelay = 1000;
+    const maxDelay = 32000;
+    const policy = new reconnection.ExponentialReconnectionPolicy(baseDelay, maxDelay, false);
+
+    const schedule1 = policy.newSchedule();
+    let value = schedule1.next().value;
+    assert.ok(value >= baseDelay && value < baseDelay * 1.15, `Value is unexpected: ${value}`);
+    value = schedule1.next().value;
+    assert.ok(value >= 2 * 0.85 * baseDelay, `Value is unexpected: ${value}`);
+
+    // Validate that the other iterator starts from the beginning
+    const schedule2 = policy.newSchedule();
+    value = schedule2.next().value;
+    assert.ok(value >= baseDelay && value < baseDelay * 1.15, `Value is unexpected: ${value}`);
+  });
+
+  it('should support starting with no delay', () => {
+    const baseDelay = 1000;
+    const maxDelay = 32000;
+    const policy = new reconnection.ExponentialReconnectionPolicy(baseDelay, maxDelay, true);
+
+    const schedule1 = policy.newSchedule();
+    let value = schedule1.next().value;
+    // Initially is zero
+    assert.strictEqual(value, 0);
+
+    // The following times follows exponential growth
+    value = schedule1.next().value;
+    assert.ok(value >= 0.85 * baseDelay && value < baseDelay * 1.15, `Value is unexpected: ${value}`);
+    value = schedule1.next().value;
+    assert.ok(value >= 2 * 0.85 * baseDelay, `Value is unexpected: ${value}`);
   });
 
   describe('#getOptions()', () => {

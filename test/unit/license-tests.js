a58f4922dfd325be8e090dd71938236604dc1243
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2019-08-19 13:39:40 +0200

    Update license headers to Apache License

--- test/unit/license-tests.js
+++ test/unit/license-tests.js
@@ -1,8 +1,17 @@
-/**
+/*
  * Copyright DataStax, Inc.
  *
- * Please see the license for details:
- * http://www.datastax.com/terms/datastax-dse-driver-license-terms
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 'use strict';
 const assert = require("assert");
@@ -10,9 +19,22 @@ const path = require("path");
 const fs = require("fs");
 
 const licenseHeaderRegex = new RegExp(
-  '^\\/\\*\\*\n \\* Copyright (?:\\(C\\) )?(?:\\d{4}(?:\\-\\d{4})? )?DataStax, Inc\\.\\n \\*\\n' +
-  ' \\* Please see the license for details:\\n' +
-  ' \\* http://www.datastax.com/terms/datastax-dse-driver-license-terms');
+  `/\\*
+ \\* Copyright DataStax, Inc\\.
+ \\*
+ \\* Licensed under the Apache License, Version 2\\.0 \\(the "License"\\);
+ \\* you may not use this file except in compliance with the License\\.
+ \\* You may obtain a copy of the License at
+ \\*
+ \\* http://www\\.apache\\.org/licenses/LICENSE-2\\.0
+ \\*
+ \\* Unless required by applicable law or agreed to in writing, software
+ \\* distributed under the License is distributed on an "AS IS" BASIS,
+ \\* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\\.
+ \\* See the License for the specific language governing permissions and
+ \\* limitations under the License\\.
+ \\*/`
+);
 
 describe('All source files', function() {
   this.timeout(5000);

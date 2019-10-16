4b448a61d3775e6791e6bf0f9f5752184b241617
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2019-10-16 11:47:32 +0200

    Skip cloud tests on AppVeyor

--- test/integration/short/cloud/cloud-tests.js
+++ test/integration/short/cloud/cloud-tests.js
@@ -33,7 +33,12 @@ const port = 9042;
 vdescribe('3.11', 'Cloud support', function () {
   // Only run tests with few versions of DSE as SNI project has a fixed C*/DSE version
 
-  this.timeout(240000);
+  if (helper.isWin()) {
+    // Skip altogether for AppVeyor
+    return;
+  }
+
+  this.timeout(300000);
 
   context('with a 3 node cluster', () => {
 

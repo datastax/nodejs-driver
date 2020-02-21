95273af3981ebb4202733cc71e0658ec31f34235
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2020-02-21 12:43:52 +0100

    Refactor more tests

--- test/integration/short/timeout-simulator-tests.js
+++ test/integration/short/timeout-simulator-tests.js
@@ -23,70 +23,109 @@ const helper = require('../../test-helper');
 const simulacron = require('../simulacron');
 const { OrderedLoadBalancingPolicy } = helper;
 
+const queryDelayedOnNode0 = 'INSERT INTO paused_on_first_node';
+const queryDelayedOnAllNodes = 'INSERT INTO paused_on_all_nodes';
+const queryDelayedOnPrepareOnNode0 = 'INSERT INTO paused_prepare_on_first_node';
+const queryDelayedOnPrepareOnAllNodes = 'INSERT INTO paused_prepare_on_all_nodes';
+
+let simulacronCluster = null;
+const readTimeout = 100;
+
 describe('client read timeouts', function () {
+  this.timeout(10000);
 
   before(done => simulacron.start(done));
   after(done => simulacron.stop(done));
 
+  beforeEach(done => {
+    simulacronCluster = new simulacron.SimulacronCluster();
+    simulacronCluster.register([3], {}, done);
+  });
+
+  beforeEach(done => simulacronCluster.clear(done));
+
+  beforeEach(done => simulacronCluster.node(0).prime({
+    when: { request: 'batch', queries: [{ query: queryDelayedOnNode0 }] },
+    then: { result: 'success', delay_in_ms: 1000 }
+  }, done));
+
+  beforeEach(done => simulacronCluster.node(0).prime({
+    when: { query: queryDelayedOnNode0 },
+    then: { result: 'success', delay_in_ms: 1000 }
+  }, done));
+
+  beforeEach(done => simulacronCluster.prime({
+    when: { request: 'batch', queries: [{ query: queryDelayedOnAllNodes }] },
+    then: { result: 'success', delay_in_ms: 1000 }
+  }, done));
+
+  beforeEach(done => simulacronCluster.prime({
+    when: { query: queryDelayedOnAllNodes },
+    then: { result: 'success', delay_in_ms: 1000 }
+  }, done));
+
+  beforeEach(done => simulacronCluster.prime({
+    when: { query: queryDelayedOnPrepareOnAllNodes },
+    then: { result: 'success', delay_in_ms: 1000, ignore_on_prepare: false }
+  }, done));
+
+  beforeEach(done => simulacronCluster.node(0).prime({
+    when: { query: queryDelayedOnPrepareOnNode0 },
+    then: { result: 'success', delay_in_ms: 1000, ignore_on_prepare: false }
+  }, done));
+
+  afterEach(done => simulacronCluster.unregister(done));
+
+  it('should do nothing else than waiting when socketOptions.readTimeout is not set',
+    testNodeUsedAsCoordinator(0));
+
+  it('should move to next host by default for simple queries',
+    testNodeUsedAsCoordinator(1, { readTimeout }));
+
+  it('should move to next host for prepared queries executions',
+    testNodeUsedAsCoordinator(1, { readTimeout, prepare: true }));
+
+  it('should move to next host for prepared requests',
+    testNodeUsedAsCoordinator(1, { readTimeout, prepare: true }), queryDelayedOnPrepareOnNode0);
+
+  it('should throw error when isIdempotent is false', async () => {
+    const client = newInstance();
+    await client.connect();
+    await helper.assertThrowsAsync(
+      client.execute(queryDelayedOnNode0, [], { isIdempotent: false, readTimeout }),
+      errors.OperationTimedOutError);
+    await client.shutdown();
+  });
+
+  //TODO
+  it('defunct the connection when the threshold passed');
+
+  //TODO
+  it('should move to next host for eachRow() executions');
 
   describe('with prepared batches', function () {
 
-    let simulacronCluster = null;
-
-    const queryDelayedOnPrepare = 'INSERT INTO paused_prepared';
-    const queryDelayedOnNode0 = 'INSERT INTO a.b (c) VALUES (?)';
-    const queryDelayedOnAllNodes = 'INSERT INTO d.e (f) VALUES (?)';
-
-    beforeEach(done => {
-      simulacronCluster = new simulacron.SimulacronCluster();
-      simulacronCluster.register([3], {}, done);
+    //TODO: Eval if needed
+    const getOptions = (readTimeout) => ({
+      socketOptions: { readTimeout }
     });
 
-    beforeEach(done => simulacronCluster.clear(done));
-
-    beforeEach(done => simulacronCluster.node(0).prime({
-      when: { request: 'batch', queries: [{ query: queryDelayedOnNode0 }] },
-      then: { result: 'success', delay_in_ms: 2000 }
-    }, done));
-
-    beforeEach(done => simulacronCluster.prime({
-      when: { request: 'batch', queries: [{ query: queryDelayedOnAllNodes }] },
-      then: { result: 'success', delay_in_ms: 2000 }
-    }, done));
-
-    beforeEach(done => simulacronCluster.prime({
-      when: { query: queryDelayedOnPrepare },
-      then: { result: 'success', delay_in_ms: 2000, ignore_on_prepare: false }
-    }, done));
-
-    afterEach(done => simulacronCluster.unregister(done));
-
     it('should retry when preparing multiple queries', async () => {
-      const client = newInstance({
-        contactPoints: simulacronCluster.getContactPoints(),
-        policies: { loadBalancing: new OrderedLoadBalancingPolicy(simulacronCluster) },
-        socketOptions: { readTimeout: 500 },
-        queryOptions: { isIdempotent: true }
-      });
+      const client = newInstance(getOptions(500));
 
       await client.connect();
-      const rs = await client.batch([ { query: queryDelayedOnNode0, params: ['a'] } ], { prepare: true });
+      const rs = await client.batch([ { query: queryDelayedOnNode0 } ], { prepare: true });
       // It timed out and then it was retried on the second host
       assert.strictEqual(rs.info.queriedHost, simulacronCluster.node(1).address);
       await client.shutdown();
     });
 
     it('should produce a NoHostAvailableError when execution timed out on all hosts', async () => {
-      const client = newInstance({
-        contactPoints: simulacronCluster.getContactPoints(),
-        policies: { loadBalancing: new OrderedLoadBalancingPolicy(simulacronCluster) },
-        socketOptions: { readTimeout: 50 },
-        queryOptions: { isIdempotent: true }
-      });
+      const client = newInstance(getOptions(50));
 
       await client.connect();
       const err = await helper.assertThrowsAsync(
-        client.batch([ { query: queryDelayedOnAllNodes, params: ['a'] } ], { prepare: true }),
+        client.batch([ { query: queryDelayedOnAllNodes } ], { prepare: true }),
         errors.NoHostAvailableError);
 
       assert.lengthOf(Object.values(err.innerErrors), 3);
@@ -98,22 +137,17 @@ describe('client read timeouts', function () {
     });
 
     it('should produce a NoHostAvailableError when prepare tried and timed out on all hosts', async () => {
-      const client = newInstance({
-        contactPoints: simulacronCluster.getContactPoints(),
-        policies: { loadBalancing: new OrderedLoadBalancingPolicy(simulacronCluster) },
-        socketOptions: { readTimeout: 50 },
-        queryOptions: { isIdempotent: true }
-      });
+      const client = newInstance(getOptions(50));
 
       await client.connect();
       // Throws on prepare
       await helper.assertThrowsAsync(
-        client.execute(queryDelayedOnPrepare, [ 'a' ], { prepare: true }),
+        client.execute(queryDelayedOnPrepareOnAllNodes, [ 'a' ], { prepare: true }),
         errors.NoHostAvailableError);
 
 
       await helper.assertThrowsAsync(
-        client.batch([ { query: queryDelayedOnPrepare, params: [ 'a' ]} ], { prepare: true }),
+        client.batch([ { query: queryDelayedOnPrepareOnAllNodes, params: [ 'a' ]} ], { prepare: true }),
         errors.NoHostAvailableError);
 
       await client.shutdown();
@@ -122,5 +156,29 @@ describe('client read timeouts', function () {
 });
 
 function newInstance(options) {
-  return helper.shutdownAfterThisTest(new Client(Object.assign({ localDataCenter: 'dc1' }, options)));
+  const client = new Client(Object.assign({
+    localDataCenter: 'dc1',
+    contactPoints: simulacronCluster.getContactPoints(),
+    // Use a LBP that yields the hosts in deterministic order for test purposes
+    policies: { loadBalancing: new OrderedLoadBalancingPolicy(simulacronCluster) },
+    queryOptions: { isIdempotent: true }
+  }, options));
+
+  helper.shutdownAfterThisTest(client);
+
+  return client;
+}
+
+function testNodeUsedAsCoordinator(nodeIndex, queryOptions, query) {
+  const prepare = false;
+  query = query || queryDelayedOnNode0;
+  return async () => {
+    const client = newInstance();
+    await client.connect();
+
+    const rs = await client.execute(query, [], Object.assign({ prepare }, queryOptions));
+    assert.strictEqual(rs.info.queriedHost, simulacronCluster.node(nodeIndex).address);
+
+    await client.shutdown();
+  };
 }

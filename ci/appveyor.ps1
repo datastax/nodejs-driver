0e19bc98b3ebb7c06cab44c1cf2798ae06619126
weideng1 <weideng1@users.noreply.github.com>
2023-09-14 01:47:33 -0600

    use newer mocha-appveyor-reporter

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -1,4 +1,4 @@
-$env:JAVA_HOME="C:\Program Files\Java\jdk1.8.0"
+$env:JAVA_HOME="C:\Program Files\Java\jdk1.11.0"
 $env:PYTHON="C:\Python27-x64"
 $env:PATH="$($env:PYTHON);$($env:PYTHON)\Scripts;$($env:JAVA_HOME)\bin;$($env:PATH)"
 $env:CCM_PATH="C:\Users\appveyor\ccm"

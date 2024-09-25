ec4d008aa69950379c8b8ad0e2d6c5d48b888cb3
janehe <jane.he@datastax.com>
2024-09-25 05:15:08 +0000

    most simple vector integration test working

--- test/integration/short/vector-tests.js
+++ test/integration/short/vector-tests.js
@@ -20,7 +20,7 @@ const helper = require('../../test-helper.js');
 const Client = require('../../../lib/client.js');
 const ExecutionProfile = require('../../../lib/execution-profile.js').ExecutionProfile;
 
-const types = require('../../../lib/types/index.js');
+const { types } = require('../../../index.js');
 const utils = require('../../../lib/utils.js');
 const errors = require('../../../lib/errors.js');
 const vit = helper.vit;
@@ -33,7 +33,7 @@ vdescribe('5.0.0', 'Vector tests', function () {
     describe('#execute with vectors', function () {
         const keyspace = helper.getRandomName('ks');
         const table = keyspace + '.' + helper.getRandomName('table');
-        const createTableCql = `CREATE TABLE ${table} (id uuid PRIMARY KEY, v1 vector<float>)`;
+        const createTableCql = `CREATE TABLE ${table} (id uuid PRIMARY KEY, v1 vector<float, 3>);`;
     
         const setupInfo = helper.setup(1, {
           keyspace: keyspace,
@@ -43,9 +43,21 @@ vdescribe('5.0.0', 'Vector tests', function () {
             const client = setupInfo.client;
             // if client undefined, raise error
             if(!client) return done(new Error('client is not defined'));
-            const id = types.types.Uuid.random();
-            const v1 = new types.Vector([1.1, 2.2, 3.3]);
-            done();
+            const id = types.Uuid.random();
+            const v1 = new Float32Array([1.1, 2.2, 3.3]);
+            const query = `INSERT INTO ${table} (id, v1) VALUES (${id}, [${v1.toString()}])`;
+            client.execute(query, [], {}, function(err){
+                if (err) return done(err);
+                client.execute(`SELECT v1 FROM ${table} WHERE id = ?`, [id], { prepare: true }, function(err, result){
+                    if (err) return done(err);
+                    assert.strictEqual(result.rows.length, 1);
+                    assert.strictEqual(result.rows[0].v1.length, 3);
+                    assert.strictEqual(result.rows[0].v1[0], v1[0]);
+                    assert.strictEqual(result.rows[0].v1[1], v1[1]);
+                    assert.strictEqual(result.rows[0].v1[2], v1[2]);
+                    done();
+                });
+            });
         });
     });
 });

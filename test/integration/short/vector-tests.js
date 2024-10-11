03a4432a9455348649426be214793949b8f9330a
janehe <jane.he@datastax.com>
2024-10-11 19:14:50 +0000

    trying to add var size vint, not working yet

--- test/integration/short/vector-tests.js
+++ test/integration/short/vector-tests.js
@@ -18,6 +18,7 @@ const assert = require('assert');
 const helper = require('../../test-helper.js');
 
 const { types } = require('../../../index.js');
+const Vector = require('../../../lib/types/vector.js');
 const vdescribe = helper.vdescribe;
 
 vdescribe('5.0.0', 'Vector tests', function () {
@@ -25,7 +26,7 @@ vdescribe('5.0.0', 'Vector tests', function () {
   describe('#execute with vectors', function () {
     const keyspace = helper.getRandomName('ks');
     const table = keyspace + '.' + helper.getRandomName('table');
-    const createTableCql = `CREATE TABLE ${table} (id uuid PRIMARY KEY, v1 vector<float, 3>);`;
+    const createTableCql = `CREATE TABLE ${table} (id uuid PRIMARY KEY, v1 vector<float, 3>, v2 vector<text,3>);`;
     
     const setupInfo = helper.setup(1, {
       keyspace: keyspace,
@@ -41,6 +42,28 @@ vdescribe('5.0.0', 'Vector tests', function () {
       client.execute(query, [id, v1], {prepare : true}, function(err){
         if (err) {return done(err);}
         client.execute(`SELECT v1 FROM ${table} WHERE id = ?`, [id], { prepare: true }, function(err, result){
+          if (err) {return done(err);}
+          assert.strictEqual(result.rows.length, 1);
+          assert.strictEqual(result.rows[0].v1.length, 3);
+          assert.strictEqual(result.rows[0].v1[0], v1[0]); 
+          assert.strictEqual(result.rows[0].v1[1], v1[1]);
+          assert.strictEqual(result.rows[0].v1[2], v1[2]);
+          done();
+        });
+      });
+    });
+
+
+    it('should insert and select vector of text', function(done){
+      const client = setupInfo.client;
+      // if client undefined, raise error
+      if(!client) {return done(new Error('client is not defined'));}
+      const id = types.Uuid.random();
+      const v1 = new Vector(['ab', 'b', 'cde'], 'text');
+      const query = `INSERT INTO ${table} (id, v2) VALUES (?, ?)`;
+      client.execute(query, [id, v1], {prepare : true}, function(err){
+        if (err) {return done(err);}
+        client.execute(`SELECT v2 FROM ${table} WHERE id = ?`, [id], { prepare: true }, function(err, result){
           if (err) {return done(err);}
           assert.strictEqual(result.rows.length, 1);
           assert.strictEqual(result.rows[0].v1.length, 3);

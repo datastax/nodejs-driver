0c3e490bb10e23b2a3271e0ebfd18c48795cda5c
janehe <siyaoh4@uci.edu>
2025-03-18 00:01:27 -0700

    search and geometry

--- lib/geometry/geometry.ts
+++ lib/geometry/geometry.ts
@@ -14,120 +14,122 @@
  * limitations under the License.
  */
 
-
 const endianness = {
   '0': 'BE',
   '1': 'LE'
 };
 
-function Geometry() {
+class Geometry {
+  static types = {
+    Point2D: 1,
+    LineString: 2,
+    Polygon: 3
+  } as const;
 
+  /**
+   * @protected
+   * @param {Number} code
+   * @returns {String}
+   * @ignore
+   */
+  static getEndianness(code: number): string {
+    const value = endianness[code.toString()];
+    if (typeof value === 'undefined') {
+      throw new TypeError('Invalid endianness with code ' + code);
+    }
+    return value;
+  }
+
+  /**
+   * Reads an int32 from binary representation based on endianness.
+   * @protected
+   * @param {Buffer} buffer
+   * @param {String} endianness
+   * @param {Number} offset
+   * @returns Number
+   * @ignore
+   */
+  static readInt32(buffer: Buffer, endianness: string, offset: number): number {
+    if (endianness === 'BE') {
+      // Old Node.js versions, e.g. v8, has buf.readInt32BE(offset[, noAssert])
+      // Newer versions do not have the noAssert parameter anymore
+      return buffer.readInt32BE(offset);
+    }
+    return buffer.readInt32LE(offset);
+  }
+
+  /**
+   * Reads a 64-bit double from binary representation based on endianness.
+   * @protected
+   * @param {Buffer} buffer
+   * @param {String} endianness
+   * @param {Number} offset
+   * @returns Number
+   * @ignore
+   */
+  static readDouble(buffer: Buffer, endianness: string, offset: number): number {
+    if (endianness === 'BE') {
+      return buffer.readDoubleBE(offset);
+    }
+    return buffer.readDoubleLE(offset);
+  }
+
+  /**
+   * Writes a 32-bit integer to binary representation based on OS endianness.
+   * @protected
+   * @param {Number} val
+   * @param {Buffer} buffer
+   * @param {Number} offset
+   * @ignore
+   */
+  writeInt32(val: number, buffer: Buffer, offset: number): void {
+    if (this.useBESerialization()) {
+      buffer.writeInt32BE(val, offset);
+    } else {
+      buffer.writeInt32LE(val, offset);
+    }
+  }
+
+  /**
+   * Writes a 64-bit double to binary representation based on OS endianness.
+   * @protected
+   * @param {Number} val
+   * @param {Buffer} buffer
+   * @param {Number} offset
+   * @ignore
+   */
+  writeDouble(val: number, buffer: Buffer, offset: number): void {
+    if (this.useBESerialization()) {
+      buffer.writeDoubleBE(val, offset);
+    } else {
+      buffer.writeDoubleLE(val, offset);
+    }
+  }
+
+  /**
+   * Writes an 8-bit int that represents the OS endianness.
+   * @protected
+   * @param {Buffer} buffer
+   * @param {Number} offset
+   * @ignore
+   */
+  writeEndianness(buffer: Buffer, offset: number): void {
+    if (this.useBESerialization()) {
+      buffer.writeInt8(0, offset);
+    } else {
+      buffer.writeInt8(1, offset);
+    }
+  }
+
+  /**
+   * Returns true if the serialization must be done in big-endian format.
+   * Designed to allow injection of OS endianness.
+   * @abstract
+   * @ignore
+   */
+  useBESerialization(): boolean {
+    throw new Error('Not Implemented');
+  }
 }
-
-Geometry.types = {
-  Point2D: 1,
-  LineString: 2,
-  Polygon: 3
-};
-
-/**
- * @protected
- * @param {Number} code
- * @returns {String}
- * @ignore
- */
-Geometry.getEndianness = function (code) {
-  const value = endianness[code.toString()];
-  if (typeof value === 'undefined') {
-    throw new TypeError('Invalid endianness with code ' + code);
-  }
-  return value;
-};
-
-/**
- * Reads an int32 from binary representation based on endianness.
- * @protected
- * @param {Buffer} buffer
- * @param {String} endianness
- * @param {Number} offset
- * @returns Number
- * @ignore
- */
-Geometry.readInt32 = function (buffer, endianness, offset) {
-  if (endianness === 'BE') {
-    return buffer.readInt32BE(offset, true);
-  }
-  return buffer.readInt32LE(offset, true);
-};
-
-/**
- * Reads an 64-bit double from binary representation based on endianness.
- * @protected
- * @param {Buffer} buffer
- * @param {String} endianness
- * @param {Number} offset
- * @returns Number
- * @ignore
- */
-Geometry.readDouble = function (buffer, endianness, offset) {
-  if (endianness === 'BE') {
-    return buffer.readDoubleBE(offset, true);
-  }
-  return buffer.readDoubleLE(offset, true);
-};
-
-/**
- * Writes an 32-bit integer to binary representation based on OS endianness.
- * @protected
- * @param {Number} val
- * @param {Buffer} buffer
- * @param {Number} offset
- * @ignore
- */
-Geometry.prototype.writeInt32 = function (val, buffer, offset) {
-  if (this.useBESerialization()) {
-    return buffer.writeInt32BE(val, offset, true);
-  }
-  return buffer.writeInt32LE(val, offset, true);
-};
-
-/**
- * Writes an 64-bit double to binary representation based on OS endianness.
- * @protected
- * @param {Number} val
- * @param {Buffer} buffer
- * @param {Number} offset
- * @ignore
- */
-Geometry.prototype.writeDouble = function (val, buffer, offset) {
-  if (this.useBESerialization()) {
-    return buffer.writeDoubleBE(val, offset, true);
-  }
-  return buffer.writeDoubleLE(val, offset, true);
-};
-
-/**
- * Writes an 8-bit int that represents the OS endianness.
- * @protected
- * @param {Buffer} buffer
- * @param {Number} offset
- * @ignore
- */
-Geometry.prototype.writeEndianness = function (buffer, offset) {
-  if (this.useBESerialization()) {
-    return buffer.writeInt8(0, offset, true);
-  }
-  return buffer.writeInt8(1, offset, true);
-};
-
-/**
- * Returns true if the serialization must be done in big-endian format.
- * Designed to allow injection of OS endianness.
- * @abstract
- * @ignore
- */
-Geometry.prototype.useBESerialization = function () {
-  throw new Error('Not Implemented');
-};
 
 export default Geometry;
--- lib/geometry/point.ts
+++ lib/geometry/point.ts
@@ -17,119 +17,122 @@ import util from "util";
 import utils from "../utils";
 import Geometry from "./geometry";
 
-
-
 /**
- * Creates a new {@link Point} instance.
  * @classdesc
  * A Point is a zero-dimensional object that represents a specific (X,Y)
  * location in a two-dimensional XY-Plane. In case of Geographic Coordinate
  * Systems, the X coordinate is the longitude and the Y is the latitude.
- * @param {Number} x The X coordinate.
- * @param {Number} y The Y coordinate.
  * @extends {Geometry}
  * @alias module:geometry~Point
- * @constructor
  */
-function Point(x, y) {
-  if (typeof x !== 'number' || typeof y !== 'number') {
-    throw new TypeError('X and Y must be numbers');
-  }
-  if (isNaN(x) || isNaN(y)) {
-    throw new TypeError('X and Y must be numbers');
-  }
+class Point extends Geometry {
+  x: number;
+  y: number;
+
   /**
-   * Returns the X coordinate of this 2D point.
-   * @type {Number}
+   * Creates a new {@link Point} instance.
+   * @param {Number} x The X coordinate.
+   * @param {Number} y The Y coordinate.
    */
-  this.x = x;
+  constructor(x: number, y: number) {
+    super();
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      throw new TypeError('X and Y must be numbers');
+    }
+    if (isNaN(x) || isNaN(y)) {
+      throw new TypeError('X and Y must be numbers');
+    }
+    /**
+     * Returns the X coordinate of this 2D point.
+     * @type {Number}
+     */
+    this.x = x;
+    /**
+     * Returns the Y coordinate of this 2D point.
+     * @type {Number}
+     */
+    this.y = y;
+  }
+
   /**
-   * Returns the Y coordinate of this 2D point.
-   * @type {Number}
+   * Creates a {@link Point} instance from
+   * a <a href="https://en.wikipedia.org/wiki/Well-known_text">Well-known Text (WKT)</a>
+   * representation of a 2D point.
+   * @param {Buffer} buffer
+   * @returns {Point}
    */
-  this.y = y;
-}
-
-//noinspection JSCheckFunctionSignatures
-util.inherits(Point, Geometry);
-
-/**
- * Creates a {@link Point} instance from
- * a <a href="https://en.wikipedia.org/wiki/Well-known_text">Well-known Text (WKT)</a>
- * representation of a 2D point.
- * @param {Buffer} buffer
- * @returns {Point}
- */
-Point.fromBuffer = function (buffer) {
-  if (!buffer || buffer.length !== 21) {
-    throw new TypeError('2D Point buffer should contain 21 bytes');
+  static fromBuffer(buffer: Buffer): Point {
+    if (!buffer || buffer.length !== 21) {
+      throw new TypeError('2D Point buffer should contain 21 bytes');
+    }
+    const endianness = Geometry.getEndianness(buffer.readInt8(0));
+    if (Geometry.readInt32(buffer, endianness, 1) !== Geometry.types.Point2D) {
+      throw new TypeError('Binary representation was not a point');
+    }
+    return new Point(Geometry.readDouble(buffer, endianness, 5), Geometry.readDouble(buffer, endianness, 13));
   }
-  const endianness = Geometry.getEndianness(buffer.readInt8(0, true));
-  if (Geometry.readInt32(buffer, endianness, 1) !== Geometry.types.Point2D) {
-    throw new TypeError('Binary representation was not a point');
+
+  /**
+   * Creates a {@link Point} instance from
+   * a <a href="https://en.wikipedia.org/wiki/Well-known_text">Well-known Text (WKT)</a>
+   * representation of a 2D point.
+   * @param {String} textValue
+   * @returns {Point}
+   */
+  static fromString(textValue: string): Point {
+    const wktRegex = /^POINT\s?\(([-0-9.]+) ([-0-9.]+)\)$/g;
+    const matches = wktRegex.exec(textValue);
+    if (!matches || matches.length !== 3) {
+      throw new TypeError("2D Point WKT should contain 2 coordinates");
+    }
+    return new Point(parseFloat(matches[1]), parseFloat(matches[2]));
   }
-  return new Point(Geometry.readDouble(buffer, endianness, 5), Geometry.readDouble(buffer, endianness, 13));
-};
 
-/**
- * Creates a {@link Point} instance from
- * a <a href="https://en.wikipedia.org/wiki/Well-known_text">Well-known Text (WKT)</a>
- * representation of a 2D point.
- * @param {String} textValue
- * @returns {Point}
- */
-Point.fromString = function (textValue) {
-  const wktRegex = /^POINT\s?\(([-0-9.]+) ([-0-9.]+)\)$/g;
-  const matches = wktRegex.exec(textValue);
-  if (!matches || matches.length !== 3) {
-    throw new TypeError('2D Point WTK should contain 2 coordinates');
+  /**
+   * Returns a <a href="https://en.wikipedia.org/wiki/Well-known_text#Well-known_binary">Well-known Binary</a> (WKB)
+   * representation of this instance.
+   * @returns {Buffer}
+   */
+  toBuffer(): Buffer {
+    const buffer = utils.allocBufferUnsafe(21);
+    this.writeEndianness(buffer, 0);
+    this.writeInt32(Geometry.types.Point2D, buffer, 1);
+    this.writeDouble(this.x, buffer, 5);
+    this.writeDouble(this.y, buffer, 13);
+    return buffer;
   }
-  return new Point(parseFloat(matches[1]), parseFloat(matches[2]));
-};
 
-/**
- * Returns a <a href="https://en.wikipedia.org/wiki/Well-known_text#Well-known_binary">Well-known Binary</a> (WKB)
- * representation of this instance.
- * @returns {Buffer}
- */
-Point.prototype.toBuffer = function () {
-  const buffer = utils.allocBufferUnsafe(21);
-  this.writeEndianness(buffer, 0);
-  this.writeInt32(Geometry.types.Point2D, buffer, 1);
-  this.writeDouble(this.x, buffer, 5);
-  this.writeDouble(this.y, buffer, 13);
-  return buffer;
-};
+  /**
+   * Returns true if the values of the point are the same, otherwise it returns false.
+   * @param {Point} other
+   * @returns {Boolean}
+   */
+  equals(other: Point): boolean {
+    if (!(other instanceof Point)) {
+      return false;
+    }
+    return this.x === other.x && this.y === other.y;
+  }
 
-/**
- * Returns true if the values of the point are the same, otherwise it returns false.
- * @param {Point} other
- * @returns {Boolean}
- */
-Point.prototype.equals = function (other) {
-  if (!(other instanceof Point)) {
+  /**
+   * Returns Well-known Text (WKT) representation of the geometry object.
+   * @returns {String}
+   */
+  toString(): string {
+    return util.format("POINT (%d %d)", this.x, this.y);
+  }
+
+  useBESerialization(): boolean {
     return false;
   }
-  return (this.x === other.x && this.y === other.y);
-};
 
-/**
- * Returns Well-known text (WKT) representation of the geometry object.
- * @returns {String}
- */
-Point.prototype.toString = function () {
-  return util.format('POINT (%d %d)', this.x, this.y);
-};
-
-Point.prototype.useBESerialization = function () {
-  return false;
-};
-
-/**
- * Returns a JSON representation of this geo-spatial type.
- */
-Point.prototype.toJSON = function () {
-  return { type: 'Point', coordinates: [ this.x, this.y ]};
-};
+  /**
+   * Returns a JSON representation of this geo-spatial type.
+   * @returns {Object}
+   */
+  toJSON(): object {
+    return { type: "Point", coordinates: [this.x, this.y] };
+  }
+}
 
 export default Point;
